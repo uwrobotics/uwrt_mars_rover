e4e247e2becd1f6fdd61e901899b31b6fe2ffb5e
Melvin Wang <melvin.mc.wang@gmail.com>
2020-02-09 22:22:17 -0500

    closes #42: use transmission interface for drivetrain

--- uwrt_mars_rover_hw/src/uwrt_mars_rover_hw_drivetrain.cpp
+++ uwrt_mars_rover_hw/src/uwrt_mars_rover_hw_drivetrain.cpp
@@ -2,75 +2,140 @@
 
 namespace uwrt_mars_rover_hw {
 
-bool UWRTRoverHWDrivetrain::init(ros::NodeHandle& /*root_nh*/, ros::NodeHandle& robot_hw_nh) {
-  bool param_fetched = robot_hw_nh.getParam("joints", joint_names_);
-  if (!param_fetched) {
-    ROS_WARN_STREAM_NAMED(name_, robot_hw_nh.getNamespace()
-                                     << "/joints could not be found and loaded from parameter server.");
-    // TODO fail init?
-  } else {
-    ROS_DEBUG_STREAM_NAMED(name_, robot_hw_nh.getNamespace() << "/joints loaded from parameter server.");
+bool UWRTRoverHWDrivetrain::init(ros::NodeHandle & /*root_nh*/, ros::NodeHandle &robot_hw_nh) {
+  if (!loadJointInfoFromParameterServer(robot_hw_nh)) {
+    return false;
   }
 
-  for (const auto& joint_name : joint_names_) {
-    hardware_interface::JointStateHandle joint_state_handle(joint_name, &joint_states[joint_name].position,
-                                                            &joint_states[joint_name].velocity,
-                                                            &joint_states[joint_name].effort);
+  for (const auto &joint_name : joint_names_) {
+    // Register JointStateHandle to the JointStateInterface
+    hardware_interface::JointStateHandle joint_state_handle(
+        joint_name, &actuator_joint_states_[joint_name].joint_position,
+        &actuator_joint_states_[joint_name].joint_velocity, &actuator_joint_states_[joint_name].joint_effort);
     joint_state_interface_.registerHandle(joint_state_handle);
 
+    // Register JointHandle associated with a JointStateHandle to command interfaces
     hardware_interface::JointHandle joint_command_handle(joint_state_interface_.getHandle(joint_name),
-                                                         &joint_commands_[joint_name].data);
+                                                         &actuator_joint_commands_[joint_name].joint_data);
     joint_position_interface_.registerHandle(joint_command_handle);
     joint_velocity_interface_.registerHandle(joint_command_handle);
     joint_effort_interface_.registerHandle(joint_command_handle);
-  }
 
+    // Wrap Actuators States
+    actuator_state_data_[joint_name].position.push_back(&actuator_joint_states_[joint_name].actuator_position);
+    actuator_state_data_[joint_name].velocity.push_back(&actuator_joint_states_[joint_name].actuator_velocity);
+    actuator_state_data_[joint_name].effort.push_back(&actuator_joint_states_[joint_name].actuator_effort);
+
+    // Wrap Joint States
+    joint_state_data_[joint_name].position.push_back(&actuator_joint_states_[joint_name].joint_position);
+    joint_state_data_[joint_name].velocity.push_back(&actuator_joint_states_[joint_name].joint_velocity);
+    joint_state_data_[joint_name].effort.push_back(&actuator_joint_states_[joint_name].joint_effort);
+
+    // Wrap Actuator Commands
+    actuator_command_data_[joint_name].position.push_back(&actuator_joint_commands_[joint_name].actuator_data);
+    actuator_command_data_[joint_name].velocity.push_back(&actuator_joint_commands_[joint_name].actuator_data);
+    actuator_command_data_[joint_name].effort.push_back(&actuator_joint_commands_[joint_name].actuator_data);
+
+    // Wrap Joint Commands
+    joint_command_data_[joint_name].position.push_back(&actuator_joint_commands_[joint_name].joint_data);
+    joint_command_data_[joint_name].velocity.push_back(&actuator_joint_commands_[joint_name].joint_data);
+    joint_command_data_[joint_name].effort.push_back(&actuator_joint_commands_[joint_name].joint_data);
+
+    // Register ActuatorToJointStateHandle with wrapped state data to a ActuatorToJointStateInterface
+    transmission_interface::ActuatorToJointStateHandle actuator_to_joint_state_handle(
+        joint_name, &joint_transmissions_.find(joint_name)->second, actuator_state_data_[joint_name],
+        joint_state_data_[joint_name]);
+    actuator_to_joint_state_interface_.registerHandle(actuator_to_joint_state_handle);
+
+    // Register JointToActuatorPositionHandle with wrapped command data to a JointToActuatorPositionInterface
+    transmission_interface::JointToActuatorPositionHandle joint_to_actuator_position_handle(
+        joint_name, &joint_transmissions_.find(joint_name)->second, actuator_command_data_[joint_name],
+        joint_command_data_[joint_name]);
+    joint_to_actuator_position_interface_.registerHandle(joint_to_actuator_position_handle);
+
+    // Register JointToActuatorVelocityHandle with wrapped command data to a JointToActuatorVelocityInterface
+    transmission_interface::JointToActuatorVelocityHandle joint_to_actuator_velocity_handle(
+        joint_name, &joint_transmissions_.find(joint_name)->second, actuator_command_data_[joint_name],
+        joint_command_data_[joint_name]);
+    joint_to_actuator_velocity_interface_.registerHandle(joint_to_actuator_velocity_handle);
+
+    // Register JointToActuatorEffortHandle with wrapped command data to a JointToActuatorEffortInterface
+    transmission_interface::JointToActuatorEffortHandle joint_to_actuator_effort_handle(
+        joint_name, &joint_transmissions_.find(joint_name)->second, actuator_command_data_[joint_name],
+        joint_command_data_[joint_name]);
+    joint_to_actuator_effort_interface_.registerHandle(joint_to_actuator_effort_handle);
+  }
   registerInterface(&joint_state_interface_);
   registerInterface(&joint_position_interface_);
   registerInterface(&joint_velocity_interface_);
-  registerInterface(&joint_effort_interface_);
+  //    registerInterface(&joint_effort_interface_); // TODO: implement in uwrt_mars_rover_hw_drivetrain_real write
 
   return true;
 }
 
-void UWRTRoverHWDrivetrain::read(const ros::Time& /*time*/, const ros::Duration& /*period*/) {
+void UWRTRoverHWDrivetrain::read(const ros::Time & /*time*/, const ros::Duration & /*period*/) {
   ROS_ERROR_STREAM_NAMED(name_, "read function called from "
                                     << name_ << " class. read calls should only happen to the overloaded function.");
 }
-void UWRTRoverHWDrivetrain::write(const ros::Time& /*time*/, const ros::Duration& /*period*/) {
+
+void UWRTRoverHWDrivetrain::write(const ros::Time & /*time*/, const ros::Duration & /*period*/) {
   ROS_ERROR_STREAM_NAMED(name_, "write function called from "
                                     << name_ << " class. write calls should only happen to the overloaded function.");
 }
 
-void UWRTRoverHWDrivetrain::doSwitch(const std::list<hardware_interface::ControllerInfo>& start_list,
-                                     const std::list<hardware_interface::ControllerInfo>& stop_list) {
+void UWRTRoverHWDrivetrain::doSwitch(const std::list<hardware_interface::ControllerInfo> &start_list,
+                                     const std::list<hardware_interface::ControllerInfo> &stop_list) {
   // Reset commands for joints claimed by stopping controllers
-  for (const auto& controller : stop_list) {
-    for (const auto& hardware_interface_resource_list : controller.claimed_resources) {
-      for (const auto& joint_name : hardware_interface_resource_list.resources) {
-        joint_commands_[joint_name].type = DrivetrainJointCommand::Type::NONE;
-        joint_commands_[joint_name].data = 0.0;
+  for (const auto &controller : stop_list) {
+    for (const auto &hardware_interface_resource_list : controller.claimed_resources) {
+      for (const auto &joint_name : hardware_interface_resource_list.resources) {
+        actuator_joint_commands_[joint_name].type = DrivetrainActuatorJointCommand::Type::NONE;
+        actuator_joint_commands_[joint_name].joint_data = 0.0;
       }
     }
   }
 
   // Set command type for joints claimed by starting controllers
-  for (const auto& controller : start_list) {
-    for (const auto& claimed : controller.claimed_resources) {
-      for (const auto& joint_name : claimed.resources) {
+  for (const auto &controller : start_list) {
+    for (const auto &claimed : controller.claimed_resources) {
+      for (const auto &joint_name : claimed.resources) {
         if (claimed.hardware_interface == "hardware_interface::PositionJointInterface") {
-          joint_commands_[joint_name].type = DrivetrainJointCommand::Type::POSITION;
-          joint_commands_[joint_name].data = joint_states[joint_name].position;
+          actuator_joint_commands_[joint_name].type = DrivetrainActuatorJointCommand::Type::POSITION;
+          actuator_joint_commands_[joint_name].actuator_data = actuator_joint_states_[joint_name].actuator_position;
         } else if (claimed.hardware_interface == "hardware_interface::VelocityJointInterface") {
-          joint_commands_[joint_name].type = DrivetrainJointCommand::Type::VELOCITY;
-          joint_commands_[joint_name].data = 0.0;
+          actuator_joint_commands_[joint_name].type = DrivetrainActuatorJointCommand::Type::VELOCITY;
+          actuator_joint_commands_[joint_name].actuator_data = 0.0;
         } else if (claimed.hardware_interface == "hardware_interface::EffortJointInterface") {
-          joint_commands_[joint_name].type = DrivetrainJointCommand::Type::EFFORT;
-          joint_commands_[joint_name].data = 0.0;
+          actuator_joint_commands_[joint_name].type = DrivetrainActuatorJointCommand::Type::EFFORT;
+          actuator_joint_commands_[joint_name].actuator_data = 0.0;
         }  // TODO add an open loop interface (voltage?)
       }
     }
   }
+}
+
+bool UWRTRoverHWDrivetrain::loadJointInfoFromParameterServer(ros::NodeHandle &robot_hw_nh) {
+  XmlRpc::XmlRpcValue joints_list;
+  bool param_fetched = robot_hw_nh.getParam("joints", joints_list);
+  if (!param_fetched) {
+    ROS_WARN_STREAM_NAMED(name_, robot_hw_nh.getNamespace() << "/joints could not be loaded from parameter server.");
+    return false;
+  }
+  ROS_DEBUG_STREAM_NAMED(name_, robot_hw_nh.getNamespace() << "/joints loaded from parameter server.");
+  ROS_ASSERT(joints_list.getType() == XmlRpc::XmlRpcValue::TypeArray);
+
+  // NOLINTNEXTLINE(modernize-loop-convert): iterator only valid for XmlRpcValue::TypeStruct
+  for (size_t joint_index = 0; joint_index < joints_list.size(); joint_index++) {
+    ROS_ASSERT(joints_list[joint_index].getType() == XmlRpc::XmlRpcValue::TypeStruct);
+    ROS_ASSERT(joints_list[joint_index].hasMember("name"));
+    ROS_ASSERT(joints_list[joint_index]["name"].getType() == XmlRpc::XmlRpcValue::TypeString);
+    std::string joint_name = joints_list[joint_index]["name"];
+    joint_names_.push_back(joint_name);
+    ROS_ASSERT(joints_list[joint_index].hasMember("transmission_reduction"));
+    ROS_ASSERT(joints_list[joint_index]["transmission_reduction"].getType() == XmlRpc::XmlRpcValue::TypeDouble);
+    joint_transmissions_.emplace(joint_name, joints_list[joint_index]["transmission_reduction"]);
+  }
+  return true;
 }
 
 }  // namespace uwrt_mars_rover_hw
--- uwrt_mars_rover_hw/src/uwrt_mars_rover_hw_drivetrain_real.cpp
+++ uwrt_mars_rover_hw/src/uwrt_mars_rover_hw_drivetrain_real.cpp
@@ -4,8 +4,12 @@
 
 namespace uwrt_mars_rover_hw {
 
-bool UWRTRoverHWDrivetrainReal::init(ros::NodeHandle& root_nh, ros::NodeHandle& robot_hw_nh) {
+bool UWRTRoverHWDrivetrainReal::init(ros::NodeHandle &root_nh, ros::NodeHandle &robot_hw_nh) {
   if (!UWRTRoverHWDrivetrain::init(root_nh, robot_hw_nh)) {
+    return false;
+  }
+
+  if (!loadRoboteqIndicesFromParameterServer(robot_hw_nh)) {
     return false;
   }
 
@@ -15,46 +19,83 @@ bool UWRTRoverHWDrivetrainReal::init(ros::NodeHandle& root_nh, ros::NodeHandle& 
   return true;
 }
 
-void UWRTRoverHWDrivetrainReal::read(const ros::Time& /*time*/, const ros::Duration& /*period*/) {
-  for (const auto& joint_name : joint_names_) {
+void UWRTRoverHWDrivetrainReal::read(const ros::Time & /*time*/, const ros::Duration & /*period*/) {
+  for (const auto &joint_name : joint_names_) {
     // TODO: change to use tpdos
-    joint_states[joint_name].position = motor_controller_->readAbsoluteEncoderCount(joint_roboteq_index_[joint_name]);
-    joint_states[joint_name].velocity = motor_controller_->readEncoderMotorSpeed(joint_roboteq_index_[joint_name]);
-    joint_states[joint_name].effort = motor_controller_->readMotorAmps(joint_roboteq_index_[joint_name]);
+    actuator_joint_states_[joint_name].actuator_position =
+        motor_controller_->readAbsoluteEncoderCount(roboteq_actuator_index_[joint_name]);
+    actuator_joint_states_[joint_name].actuator_velocity =
+        motor_controller_->readEncoderMotorSpeed(roboteq_actuator_index_[joint_name]);
+    actuator_joint_states_[joint_name].actuator_effort =
+        motor_controller_->readMotorAmps(roboteq_actuator_index_[joint_name]);
   }
+  actuator_to_joint_state_interface_.propagate();
 }
 
-void UWRTRoverHWDrivetrainReal::write(const ros::Time& /*time*/, const ros::Duration& /*period*/) {
-  for (const auto& joint_name : joint_names_) {
+void UWRTRoverHWDrivetrainReal::write(const ros::Time & /*time*/, const ros::Duration & /*period*/) {
+  for (const auto &joint_name : joint_names_) {
     bool successful_joint_write = false;
-    switch (joint_commands_[joint_name].type) {
-      case UWRTRoverHWDrivetrain::DrivetrainJointCommand::Type::NONE:
-        ROS_WARN_STREAM_NAMED(name_, joint_name << " has a " << joint_commands_[joint_name].type << " command type.");
+    switch (actuator_joint_commands_[joint_name].type) {
+      case UWRTRoverHWDrivetrain::DrivetrainActuatorJointCommand::Type::POSITION:
+        joint_to_actuator_position_interface_.propagate();
+        successful_joint_write =
+            motor_controller_->setPosition(static_cast<int32_t>(actuator_joint_commands_[joint_name].actuator_data),
+                                           roboteq_actuator_index_[joint_name]);
         break;
 
-      case UWRTRoverHWDrivetrain::DrivetrainJointCommand::Type::POSITION:
-        successful_joint_write = motor_controller_->setPosition(static_cast<int32_t>(joint_commands_[joint_name].data),
-                                                                joint_roboteq_index_[joint_name]);
+      case UWRTRoverHWDrivetrain::DrivetrainActuatorJointCommand::Type::VELOCITY:
+        joint_to_actuator_velocity_interface_.propagate();
+        successful_joint_write =
+            motor_controller_->setVelocity(static_cast<int32_t>(actuator_joint_commands_[joint_name].actuator_data),
+                                           roboteq_actuator_index_[joint_name]);
         break;
 
-      case UWRTRoverHWDrivetrain::DrivetrainJointCommand::Type::VELOCITY:
-        successful_joint_write = motor_controller_->setVelocity(static_cast<int32_t>(joint_commands_[joint_name].data),
-                                                                joint_roboteq_index_[joint_name]);
-        break;
-
-      case UWRTRoverHWDrivetrain::DrivetrainJointCommand::Type::EFFORT:
+      case UWRTRoverHWDrivetrain::DrivetrainActuatorJointCommand::Type::EFFORT:
+        joint_to_actuator_effort_interface_.propagate();
         // TODO: look into roboteq torque control
         break;
 
+      case UWRTRoverHWDrivetrain::DrivetrainActuatorJointCommand::Type::NONE:
+        ROS_WARN_STREAM_NAMED(name_, joint_name << " has a " << actuator_joint_commands_[joint_name].type
+                                                << " command type_. Sending Stop Command to Roboteq Controller!");
+        successful_joint_write = motor_controller_->stopInAllModes(roboteq_actuator_index_[joint_name]);
+        break;
+
       default:
-        ROS_ERROR_STREAM_NAMED(name_, "Could not write to joints. "
-                                          << static_cast<int>(joint_commands_[joint_name].type)
-                                          << " is an unknown command type.");
+        ROS_ERROR_STREAM_NAMED(
+            name_, joint_name << " has a joint command with index "
+                              << static_cast<int>(actuator_joint_commands_[joint_name].type)
+                              << ",which is an unknown command type. Sending Stop Command to Roboteq Controller!");
+        successful_joint_write = motor_controller_->stopInAllModes(roboteq_actuator_index_[joint_name]);
     }
     if (!successful_joint_write) {
-      ROS_ERROR_STREAM_NAMED(
-          name_, "Failed to write " << joint_commands_[joint_name].type << " command to " << joint_name << ".");
+      ROS_ERROR_STREAM_NAMED(name_, "Failed to write " << actuator_joint_commands_[joint_name].type << " command to "
+                                                       << joint_name << ".");
     }
   }
+}
+
+bool UWRTRoverHWDrivetrainReal::loadRoboteqIndicesFromParameterServer(ros::NodeHandle &robot_hw_nh) {
+  XmlRpc::XmlRpcValue joints_list;
+  bool param_fetched = robot_hw_nh.getParam("joints", joints_list);
+  if (!param_fetched) {
+    ROS_WARN_STREAM_NAMED(name_, robot_hw_nh.getNamespace() << "/joints could not be loaded from parameter server.");
+    return false;
+  }
+  ROS_DEBUG_STREAM_NAMED(name_, robot_hw_nh.getNamespace() << "/joints loaded from parameter server.");
+  ROS_ASSERT(joints_list.getType() == XmlRpc::XmlRpcValue::TypeArray);
+
+  // NOLINTNEXTLINE(modernize-loop-convert): iterator only valid for XmlRpcValue::TypeStruct
+  for (size_t joint_index = 0; joint_index < joints_list.size(); joint_index++) {
+    ROS_ASSERT(joints_list[joint_index].getType() == XmlRpc::XmlRpcValue::TypeStruct);
+    ROS_ASSERT(joints_list[joint_index].hasMember("name"));
+    ROS_ASSERT(joints_list[joint_index]["name"].getType() == XmlRpc::XmlRpcValue::TypeString);
+    std::string joint_name = joints_list[joint_index]["name"];
+
+    ROS_ASSERT(joints_list[joint_index].hasMember("roboteq_index"));
+    ROS_ASSERT(joints_list[joint_index]["roboteq_index"].getType() == XmlRpc::XmlRpcValue::TypeInt);
+    roboteq_actuator_index_[joint_name] = static_cast<int>(joints_list[joint_index]["roboteq_index"]);
+  }
+  return true;
 }
 }  // namespace uwrt_mars_rover_hw

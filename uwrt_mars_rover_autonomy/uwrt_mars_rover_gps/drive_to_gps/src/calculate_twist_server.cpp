6bae523d6ba891dcb42c289711f944222048631c
Azum Nanji <azumnanji@gmail.com>
2020-03-11 22:49:07 -0400

    format changes

--- uwrt_mars_rover_autonomy/uwrt_mars_rover_gps/drive_to_gps/src/calculate_twist_server.cpp
+++ uwrt_mars_rover_autonomy/uwrt_mars_rover_gps/drive_to_gps/src/calculate_twist_server.cpp
@@ -1,22 +1,22 @@
-#include "utils/utils.h"
+#include <actionlib/server/simple_action_server.h>
+#include <drive_to_gps/gps_goalAction.h>
+#include <ros/callback_queue.h>
 #include "drive_to_gps/heading.h"
 #include "geometry_msgs/Twist.h"
 #include "ros/ros.h"
 #include "sensor_msgs/NavSatFix.h"
-#include <actionlib/server/simple_action_server.h>
-#include <drive_to_gps/gps_goalAction.h>
-#include <ros/callback_queue.h>
+#include "utils/utils.h"
 
-#define MAX_ERROR 5         // error for distance comparisons in metres
-#define MAX_LINEAR_VEL 1.0  // max linear velocity of the rover (change)
-#define MAX_ANGULAR_VEL 1.0 // max angular velocity of the rover (change)
+#define MAX_ERROR 5          // error for distance comparisons in metres
+#define MAX_LINEAR_VEL 1.0   // max linear velocity of the rover (change)
+#define MAX_ANGULAR_VEL 1.0  // max angular velocity of the rover (change)
 
 typedef actionlib::SimpleActionServer<drive_to_gps::gps_goalAction> Server;
 
 ros::Publisher pub;
 ros::CallbackQueue my_queue;
-drive_to_gps::headingPtr curr_head = NULL; // current heading of the rover
-sensor_msgs::NavSatFixPtr curr_gps = NULL; // current gps coordinates
+drive_to_gps::headingPtr curr_head = NULL;  // current heading of the rover
+sensor_msgs::NavSatFixPtr curr_gps = NULL;  // current gps coordinates
 int degrees_to_target = 0;
 
 void execute(const drive_to_gps::gps_goalGoalConstPtr &goal, Server *as) {
@@ -29,20 +29,16 @@ void execute(const drive_to_gps::gps_goalGoalConstPtr &goal, Server *as) {
       r.sleep();
     }
   }
-  sensor_msgs::NavSatFixPtr gps_goal(
-      new sensor_msgs::NavSatFix(goal->gps_goal));
+  sensor_msgs::NavSatFixPtr gps_goal(new sensor_msgs::NavSatFix(goal->gps_goal));
   bool end = false;
   ros::Rate rate(1);
-  while (calculate_distance(gps_goal, curr_gps) > MAX_ERROR && !end &&
-         ros::ok()) {
+  while (calculate_distance(gps_goal, curr_gps) > MAX_ERROR && !end && ros::ok()) {
     ROS_INFO("Calculating degrees, heading and sending twist message");
     geometry_msgs::Twist msg;
     int goal_heading = calculate_degrees(gps_goal, curr_gps);
     int multiplier = goal_heading > curr_head->degrees ? 1 : -1;
     int heading_diff = abs(goal_heading - curr_head->degrees);
-    ROS_INFO(
-        "Heading between current heading and goal is %d and direction is %d",
-        heading_diff, multiplier);
+    ROS_INFO("Heading between current heading and goal is %d and direction is %d", heading_diff, multiplier);
     msg.angular.z = heading_diff > 4 ? multiplier * MAX_ANGULAR_VEL : 0;
     msg.linear.x = MAX_LINEAR_VEL;
     feedback.go_to_goal = msg;
@@ -57,14 +53,12 @@ void execute(const drive_to_gps::gps_goalGoalConstPtr &goal, Server *as) {
 }
 
 void update_current_heading(const drive_to_gps::headingConstPtr &c_head) {
-  if (curr_head == NULL)
-    curr_head = drive_to_gps::headingPtr(new drive_to_gps::heading);
+  if (curr_head == NULL) curr_head = drive_to_gps::headingPtr(new drive_to_gps::heading);
   curr_head->degrees = c_head->degrees;
 }
 
 void update_current_gps(const sensor_msgs::NavSatFixConstPtr &gps_current) {
-  if (curr_gps == NULL)
-    curr_gps = sensor_msgs::NavSatFixPtr(new sensor_msgs::NavSatFix);
+  if (curr_gps == NULL) curr_gps = sensor_msgs::NavSatFixPtr(new sensor_msgs::NavSatFix);
   curr_gps->latitude = gps_current->latitude;
   curr_gps->longitude = gps_current->longitude;
 }
@@ -76,12 +70,9 @@ int main(int argc, char **argv) {
   nq.setCallbackQueue(&my_queue);
   drive_to_gps::headingPtr curr_head;
   // publish current heading of the rover
-  Server server(n, "calculate_twist", boost::bind(&execute, _1, &server),
-                false);
-  ros::Subscriber sub_curr_heading =
-      nq.subscribe("/heading/curr", 1, update_current_heading);
-  ros::Subscriber sub_curr_gps =
-      nq.subscribe("/gps/fix", 1, update_current_gps);
+  Server server(n, "calculate_twist", boost::bind(&execute, _1, &server), false);
+  ros::Subscriber sub_curr_heading = nq.subscribe("/heading/curr", 1, update_current_heading);
+  ros::Subscriber sub_curr_gps = nq.subscribe("/gps/fix", 1, update_current_gps);
   server.start();
 
   ros::spin();
--- uwrt_mars_rover_autonomy/uwrt_mars_rover_gps/drive_to_gps/src/utils.cpp
+++ uwrt_mars_rover_autonomy/uwrt_mars_rover_gps/drive_to_gps/src/utils.cpp
@@ -1,29 +1,28 @@
 #include "utils/utils.h"
 
-static inline double deg_to_rad(double deg) { return M_PI / 180.0 * deg; }
+static inline double deg_to_rad(double deg) {
+  return M_PI / 180.0 * deg;
+}
 
-int calculate_degrees(const sensor_msgs::NavSatFixConstPtr &curr_gps,
-                      const sensor_msgs::NavSatFixConstPtr &prev) {
+int calculate_degrees(const sensor_msgs::NavSatFixConstPtr &curr_gps, const sensor_msgs::NavSatFixConstPtr &prev) {
   double dlong = deg_to_rad(curr_gps->longitude - prev->longitude);
   double lat1 = deg_to_rad(prev->latitude);
   double lat2 = deg_to_rad(curr_gps->latitude);
   double dx = cos(lat2) * sin(dlong);
   double dy = cos(lat1) * sin(lat2) - sin(lat1) * cos(lat2) * cos(dlong);
-  double angle = 180.0 / M_PI * atan2(dx, dy); // convert radians to degrees
+  double angle = 180.0 / M_PI * atan2(dx, dy);  // convert radians to degrees
   ROS_INFO("Angle is %lf", angle);
   int heading = (int(round(angle)) + 360) % 360;
   ROS_INFO("Heading is %d degrees", heading);
   return heading;
 }
 
-double calculate_distance(const sensor_msgs::NavSatFixConstPtr &goal,
-                          const sensor_msgs::NavSatFixConstPtr &curr) {
+double calculate_distance(const sensor_msgs::NavSatFixConstPtr &goal, const sensor_msgs::NavSatFixConstPtr &curr) {
   double lat1 = deg_to_rad(curr->latitude);
   double lat2 = deg_to_rad(goal->latitude);
   double dlat = deg_to_rad(goal->latitude - curr->latitude);
   double dlong = deg_to_rad(goal->longitude - curr->longitude);
-  double calc = sin(dlat / 2) * sin(dlat / 2) +
-                cos(lat1) * cos(lat2) * sin(dlong / 2) * sin(dlong / 2);
+  double calc = sin(dlat / 2) * sin(dlat / 2) + cos(lat1) * cos(lat2) * sin(dlong / 2) * sin(dlong / 2);
   double c = 2 * atan2(sqrt(calc), sqrt(1 - calc));
 
   return RADIUS_E * c;

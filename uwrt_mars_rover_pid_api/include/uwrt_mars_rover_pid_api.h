43c7be521d298249ac4b1241f90d0a542acb22ae
yreda1999 <yreda@uwaterloo.ca>
2020-06-01 19:06:31 -0400

    trying to compile

--- uwrt_mars_rover_pid_api/include/uwrt_mars_rover_pid_api.h
+++ uwrt_mars_rover_pid_api/include/uwrt_mars_rover_pid_api.h
@@ -1,5 +1,5 @@
 #pragma once
-#include "uwrt_can.h"
+#include "uwrt_utils/uwrt_can.h"
 #include "ros/ros.h"
 
 #define PID_API_TESTING
@@ -11,9 +11,6 @@ class pidApi {
     ros::Rate _loop_rate;
     uwrt_utils::UWRTCANWrapper CANMsg;
     void pidApi::updatePIDGain(uwrt_mars_rover_msgs::set_state::Request &req, uwrt_mars_rover_msgs::set_state::Response &res);
-    std::string stringifyParam(unit8_t param);
-    std::string stringifyVelPos(unit8_t param);
-    std::string stringifyActuatorID(unit8_t param);
     public:
     pidApi(const ros::NodeHandle &nh, uint8_t loop_rate);
     void run();
--- uwrt_mars_rover_pid_api/src/uwrt_mars_rover_pid_api.cpp
+++ uwrt_mars_rover_pid_api/src/uwrt_mars_rover_pid_api.cpp
@@ -5,6 +5,34 @@
 #else
 #define CAN_INTERFACE "can0"
 #endif
+// will be part of new repo
+std::string tringifyActuatorID(actuatorID_t actuator) {
+    switch(actuator) {
+        case(TURNTABLE): return "TURNTABLE";
+        case(SHOULDER): return"SHOULDER";
+        case(ELBOW): return "ELBOW";
+        case(WRISTLEFT): return "WRISTLEFT";
+        case(WRISTRIGHT): return "WRISTRIGHT";
+        case(CLAW) : return "CLAW";
+        default: return "ERROR";
+    }
+}
+
+std::string HW_BRIDGE::ARM::PID::stringifyParam(parameter_t param) {
+    switch(param) {
+        case(P): return "P";
+        case(I): return "I";
+        case(D): return "D";
+        case(DEADZONE): return "DEADZONE";
+        case(BIAS): return "BIAS";
+        default: return "ERROR";
+    }
+}
+
+std::string stringifyVelPos(bool vel_pos) {
+    return vel_pos ? "velocity" : "position";
+}
+// END
 
 pidApi::pidApi(const ros::NodeHandle &nh, uint8_t loop_rate) : _nh(nh), _loop_rate(loop_rate) {
     CANMsg = uwrt_utils::UWRTCANWrapper("pid_api_can", CAN_INTERFACE, true);

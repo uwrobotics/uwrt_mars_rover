53c51f6fd60f1af04de5c9963bbf257462601cae
Joshua Kurien <joshua.kurien4@gmail.com>
2020-09-23 21:35:15 -0400

    ROS naming format changes

--- uwrt_mars_rover_pid_tuning_api/include/uwrt_mars_rover_pid_tuning_api.h
+++ uwrt_mars_rover_pid_tuning_api/include/uwrt_mars_rover_pid_tuning_api.h
@@ -1,7 +1,7 @@
 #pragma once
 #include "ros/ros.h"
-#include "uwrt_mars_rover_msgs/pid_tuning_api_msg.h"
-#include "uwrt_mars_rover_msgs/pid_tuning_api_srv.h"
+#include "uwrt_mars_rover_msgs/PidTuningApi.h"
+#include "uwrt_mars_rover_msgs/pid_tuning_api.h"
 #include "uwrt_utils/uwrt_can.h"
 
 class PidTuningApi {
@@ -11,9 +11,9 @@ class PidTuningApi {
   const std::string _log_filter;
   uwrt_utils::UWRTCANWrapper CANMsg;
   // public:
-  bool updatePIDParam(uwrt_mars_rover_msgs::pid_tuning_api_srv::Request& req,
-                      uwrt_mars_rover_msgs::pid_tuning_api_srv::Response& res);
-  static bool isValidPayload(uwrt_mars_rover_msgs::pid_tuning_api_msg& payload_data);
+  bool updatePIDParam(uwrt_mars_rover_msgs::pid_tuning_api::Request& req,
+                      uwrt_mars_rover_msgs::pid_tuning_api::Response& res);
+  static bool isValidPayload(uwrt_mars_rover_msgs::PidTuningApi& payload_data);
 
  public:
   PidTuningApi(ros::NodeHandle& nh, uint8_t loop_rate, std::string log_filter = "pidApiServiceServer",
--- uwrt_mars_rover_pid_tuning_api/src/uwrt_mars_rover_pid_tuning_api.cpp
+++ uwrt_mars_rover_pid_tuning_api/src/uwrt_mars_rover_pid_tuning_api.cpp
@@ -20,16 +20,16 @@ PidTuningApi::PidTuningApi(ros::NodeHandle& nh, const uint8_t loop_rate, std::st
                                     HWBRIDGE::CANID::SET_JOINT_PID_BIAS});
 }
 
-bool PidTuningApi::updatePIDParam(uwrt_mars_rover_msgs::pid_tuning_api_srv::Request& req,
-                                  uwrt_mars_rover_msgs::pid_tuning_api_srv::Response& res) {
+bool PidTuningApi::updatePIDParam(uwrt_mars_rover_msgs::pid_tuning_api::Request& req,
+                                  uwrt_mars_rover_msgs::pid_tuning_api::Response& res) {
   ROS_INFO_NAMED(_log_filter, "Updating PID gains");
   if (!isValidPayload(req.pid_tuning_api_data)) {
     ROS_WARN_NAMED(_log_filter, "Invalid PID payload. Service call failed. Message not sent");
     return false;
   }
   HWBRIDGE::ARM::PID::tuningApiPayload payload{.value = static_cast<float>(req.pid_tuning_api_data.value),
-                                               .velocity = static_cast<bool>(req.pid_tuning_api_data.isVelocityPID),
-                                               .actuatorID = req.pid_tuning_api_data.actuatorID};
+                                               .velocity = static_cast<bool>(req.pid_tuning_api_data.is_velocity_pid),
+                                               .actuatorID = req.pid_tuning_api_data.actuator_id};
   int32_t can_id;
   std::string lower_case_parameter = boost::to_lower_copy(req.pid_tuning_api_data.parameter);
   if (lower_case_parameter == "deadzone") {
@@ -45,17 +45,17 @@ bool PidTuningApi::updatePIDParam(uwrt_mars_rover_msgs::pid_tuning_api_srv::Requ
   }
 
   bool success = CANMsg.writeToID<HWBRIDGE::ARM::PID::tuningApiPayload>(payload, can_id);
-  res.success = static_cast<uwrt_mars_rover_msgs::pid_tuning_api_srv::Response::_success_type>(success);
+  res.success = static_cast<uwrt_mars_rover_msgs::pid_tuning_api::Response::_success_type>(success);
   return success;
 }
 
-bool PidTuningApi::isValidPayload(uwrt_mars_rover_msgs::pid_tuning_api_msg& payload_data) {
+bool PidTuningApi::isValidPayload(uwrt_mars_rover_msgs::PidTuningApi& payload_data) {
   const int num_parameters = 5;
   const int highest_actuator_id = 5;
   std::array<std::string, num_parameters> parameters{"p", "i", "d", "bias", "deadzone"};
   bool exists = std::find(std::begin(parameters), std::end(parameters), boost::to_lower_copy(payload_data.parameter)) !=
                 std::end(parameters);
-  return exists && payload_data.actuatorID >= 0 && payload_data.actuatorID <= highest_actuator_id;
+  return exists && payload_data.actuator_id >= 0 && payload_data.actuator_id <= highest_actuator_id;
 }
 
 void PidTuningApi::run() {

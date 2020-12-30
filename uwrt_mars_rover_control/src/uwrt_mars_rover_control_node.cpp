dbe0599125f10337896ba8b4ce6070cbfe16c9c9
wmmc88 <melvin.mc.wang@gmail.com>
2020-12-30 16:08:42 -0500

    Better Comments

--- uwrt_mars_rover_control/src/uwrt_mars_rover_control_node.cpp
+++ uwrt_mars_rover_control/src/uwrt_mars_rover_control_node.cpp
@@ -5,6 +5,10 @@
 
 #include "uwrt_mars_rover_control/uwrt_mars_rover_hw_control_loop_real.h"
 
+/** hasRealtimeKernel - Determine whether or not kernel has realtime patch by checking /sys/kernel/realtime
+ *
+ * @return True if /sys/kernel/realtime indicates realtime kernel capabilites, else false.
+ */
 bool hasRealtimeKernel() {
   std::ifstream realtime_file("/sys/kernel/realtime", std::ios::in);
   bool has_realtime = false;
@@ -12,6 +16,12 @@ bool hasRealtimeKernel() {
   return has_realtime;
 }
 
+/** enableRealtimeScheduling - Set the kernel scheduling policy to one of the realtime policies
+ *
+ * @param node_name This node's name used for rosconsole logger name
+ * @param scheduling_policy One of the policies from http://manpages.ubuntu.com/manpages/focal/man7/sched.7.html. Should
+ * be one of the realtime policies(SCHED_FIFO, SCHED_RR)
+ */
 void enableRealtimeScheduling(const std::string& node_name, int scheduling_policy) {
   if (!hasRealtimeKernel()) {
     ROS_FATAL_STREAM_NAMED(
@@ -19,15 +29,18 @@ void enableRealtimeScheduling(const std::string& node_name, int scheduling_polic
     exit(EXIT_FAILURE);
   }
 
+  // Determine kernel's max scheduling priority
   const int max_thread_priority = sched_get_priority_max(scheduling_policy);
   if (max_thread_priority == -1) {
-    ROS_FATAL_STREAM_NAMED(node_name, "Kernel failed to provide kernel max priority!");
+    ROS_FATAL_STREAM_NAMED(node_name, "Kernel failed to provide kernel max priority information!");
     exit(EXIT_FAILURE);
   }
 
+  // Get handle to this thread
   pthread_t this_thread = pthread_self();
   struct sched_param scheduling_params = {.sched_priority = max_thread_priority};
 
+  // Set this thread to max scheduling priority and change scheduling policy
   int ret = pthread_setschedparam(this_thread, scheduling_policy, &scheduling_params);
   if (0 != ret) {
     ROS_FATAL_STREAM_NAMED(node_name, "Failed to set realtime thread priority to "
@@ -36,13 +49,13 @@ void enableRealtimeScheduling(const std::string& node_name, int scheduling_polic
     exit(EXIT_FAILURE);
   }
 
+  // Double check that this thread's policy matches what was requested
   int current_policy = -1;
   ret = pthread_getschedparam(this_thread, &current_policy, &scheduling_params);
   if (ret != 0) {
     ROS_FATAL_STREAM_NAMED(node_name, "Failed to retrieve current thread scheduling parameters. Return Code: " << ret);
     exit(EXIT_FAILURE);
   }
-
   if (scheduling_policy != current_policy) {
     ROS_FATAL_STREAM_NAMED(node_name, "Thread Scheduling policy failed to be set to "
                                           << scheduling_policy << ". Currently Set Policy: " << current_policy);
@@ -53,6 +66,10 @@ void enableRealtimeScheduling(const std::string& node_name, int scheduling_polic
                                        << scheduling_policy << " with priority " << max_thread_priority << ".");
 }
 
+/** preventMemoryPageFaults - prevent page faults caused by critical memory sections being swapped into swap space
+ *
+ * @param node_name This node's name used for rosconsole logger name
+ */
 void preventMemoryPageFaults(const std::string& node_name) {
   const int ret = mlockall(MCL_CURRENT | MCL_FUTURE);
   if (ret != 0) {
@@ -86,7 +103,6 @@ int main(int argc, char** argv) {
 
     enableRealtimeScheduling(node_name, REALTIME_SCHEDULING_POLICY);
 
-    // Prevents RAM from being swapped out
     preventMemoryPageFaults(node_name);
 
   } else {

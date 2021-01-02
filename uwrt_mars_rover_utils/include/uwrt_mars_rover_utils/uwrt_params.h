47a5a9375f3be5ffb39595823b4b378db8a053d0
wmmc88 <melvin.mc.wang@gmail.com>
2021-01-02 12:23:51 -0500

    add docstring for uwrt_mars_rover_utils::getParam

--- uwrt_mars_rover_utils/include/uwrt_mars_rover_utils/uwrt_params.h
+++ uwrt_mars_rover_utils/include/uwrt_mars_rover_utils/uwrt_params.h
@@ -5,8 +5,16 @@
 
 namespace uwrt_mars_rover_utils {
 
+/** getParam - Retrieves a ROS parameter and provides a default value if there's a failure
+ *
+ * @tparam T The type of value to load from parameter server. Must be a type supported by XMLRPC.
+ * @param nh The nodehandle to use to do the parameter lookup
+ * @param param_name The name of the parameter to lookup
+ * @param default_value The default value to return if lookup fails
+ * @param logger_name The logger name to use with rosconsole log statements
+ * @return parameter from ROS parameter server, or default_value if parameter lookup fails
+ */
 template <typename T>
-
 T getParam(const ros::NodeHandle& nh, const std::string& param_name, const T& default_value,
            const std::string& logger_name) {
   T retrieved_param;

02f3ef206839846dc0b5150bd6615bb5e201f162
Dhruv Yayhdapu <dmupadhyay12@gmail.com>
2021-01-20 09:22:10 -0500

    Added timing functionality to send can messages in 30 second intervals

--- uwrt_test_stress_can/src/main.cpp
+++ uwrt_test_stress_can/src/main.cpp
@@ -1,5 +1,8 @@
 #include <ros/ros.h>
 #include <uwrt_mars_rover_utils/uwrt_can.h>
+#include <thread>
+#include <ctime>
+
 
 void sendCANMessages() {
     constexpr uint16_t CAN_ID = 0x123;
@@ -7,6 +10,7 @@ void sendCANMessages() {
     std::string can_interface = "can0";
     uwrt_mars_rover_utils::UWRTCANWrapper can_wrapper_int = uwrt_mars_rover_utils::UWRTCANWrapper("can_stress_test", can_interface, true);
     uint16_t msg = 0;
+
     for (int16_t msg = 0; msg < ITERATIONS; msg++) {
         if (can_wrapper_int.writeToID<uint16_t>(msg, CAN_ID)) {
             if (msg%100 == 0)
@@ -18,7 +22,19 @@ void sendCANMessages() {
     }
 }
 
+void taskTimer() {
+    using namespace std::chrono;
+    std::this_thread::sleep_for(duration(seconds(30)));
+
+}
+
 int main(int argc, char* argv[]) {
+    std::thread t1(&taskTimer);
+    sendCANMessages();
+    t1.join(); // Waits for the first thread to complete it's 30 second wait before starting sending of second set
+    std::thread t2(&taskTimer); 
+    sendCANMessages(); 
+    t2.join();
+
     ros::init(argc, argv, "uwrt_test_stress_can");
-    //TODO: Implement timer to prevent the function calls from exceeding 30 sec
 }

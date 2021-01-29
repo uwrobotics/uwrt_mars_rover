c4398860f7e26322bceb0a93b5af6991461d0096
Urban Pistek <urban.pistek@gmail.com>
2021-01-28 22:15:18 -0500

    Added params for constants, used angular z and linear x for velocity commands

--- uwrt_mars_rover_autonomy/spiralSearch/src/spiralSearch_server.cpp
+++ uwrt_mars_rover_autonomy/spiralSearch/src/spiralSearch_server.cpp
@@ -11,12 +11,17 @@ Currently works with turtlesim for testing
 #include <sstream> 
 #include <cmath>
 
+//Global Variable to stop the server if need be
+bool runSpiralSearch = true; 
+
+//Global Variables
+static double angular_velocity; //this is the constant angular velocity, it controls the speed of rover as it moves around
+static double spiral_constant; //This primarily controls the space between each radius/how tight it is
+
 //Action Server variables 
 typedef actionlib::SimpleActionServer<spiralSearch::spiralSearchAction> Server;
 spiralSearch::spiralSearchFeedback feedback; 
 spiralSearch::spiralSearchResult result; 
-
-const double PI = 3.14159265359;
 
 //To publish commands to turtlesim
 ros::Publisher velocity_publisher;
@@ -37,29 +42,37 @@ void execute(const spiralSearch::spiralSearchGoalConstPtr &goal, Server* as){
     //declare vel_msg as a "Twist" type to be able to change linear and angular velocities
     geometry_msgs::Twist vel_msg;
 
-    double angular_velocity = 5; //this is the constant angular velocity, it controls the speed of rover as it moves around
-    double spiral_constant = 0.005; //This primarily controls the space between each radius/how tight it is
-
-    double Vx = 0; 
-    double Vy = 0;
-    double time = 0; 
+    //double Vx = 0; 
+    //double Vy = 0;
+    double velocity = 0;
     
     //this is the frequency that the loop will follow, by keeping track of "sleep()"
     ros::Rate loop_rate(10);
+
+    //Get ROS Time 
+    double startTime = ros::Time::now().toSec();
+    double currentTime = ros::Time::now().toSec();
+    double timeDelta = 0; 
     
     //Run until the goal has been reached
     //Add capability to be stopped
-    while( (turtlesim_pose.x < goal->goal_x) && (turtlesim_pose.y < goal->goal_y)){
+    while( (turtlesim_pose.x < goal->goal_x) && (turtlesim_pose.y < goal->goal_y) && runSpiralSearch){
         
-        time += 0.01;  //Increment time
+        //Spiral motion using cartisian coordinates and linear velocities 
+        //Vx = spiral_constant*angular_velocity*(cos(angular_velocity*time) - angular_velocity*time*sin(angular_velocity*time) );
+        //Vy = spiral_constant*angular_velocity*(sin(angular_velocity*time) - angular_velocity*time*cos(angular_velocity*time) );
 
-        //Determine Vx and Vy after each interation 
-        Vx = spiral_constant*angular_velocity*(cos(angular_velocity*time) - angular_velocity*time*sin(angular_velocity*time) );
-        Vy = spiral_constant*angular_velocity*(sin(angular_velocity*time) - angular_velocity*time*cos(angular_velocity*time) );
-        
         //this makes the linear change
-        vel_msg.linear.x = Vx;
-        vel_msg.linear.y = Vy;
+        //vel_msg.linear.x = Vx;
+        //vel_msg.linear.y = Vy;
+
+        //Use current ROS Time
+        currentTime = ros::Time::now().toSec();
+        timeDelta = currentTime - startTime; 
+
+        //Send linear and angular velocity commands
+        vel_msg.linear.x = spiral_constant*timeDelta;
+        vel_msg.angular.z = angular_velocity;  
 
         velocity_publisher.publish(vel_msg);
         ros::spinOnce();//this will call all the callbacks waiting to be called
@@ -71,8 +84,8 @@ void execute(const spiralSearch::spiralSearchGoalConstPtr &goal, Server* as){
     
     //this stops the turtle
     vel_msg.linear.x = 0;
+    vel_msg.angular.z = 0;
     velocity_publisher.publish(vel_msg);
-    //cout<<"DONE "<<endl;
 
 }
 
@@ -80,6 +93,15 @@ int main(int argc, char **argv) {
 
     ros::init(argc, argv, "spiralSearch");
     ros::NodeHandle n, nh;
+
+    nh.param("spiral_constant", spiral_constant, 0.25);
+    nh.param("angular_velocity", angular_velocity, 1.0);
+
+    //If parameters are not defined for some ensure the varaibles are not undefined
+    if(!nh.hasParam("spiral_constant") || !nh.hasParam("angular_velocity")){
+        spiral_constant = 0.1; 
+        angular_velocity = 1; 
+    }
 
     Server server(nh, "spiralSearch", boost::bind(&execute, _1, &server), false);
 

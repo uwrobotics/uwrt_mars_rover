dddda7154652039b1347804af7895d4052ca5c12
niiquaye <omarmon@uwaterloo.ca>
2021-06-06 12:59:23 -0400

    ROS2 Drivetrain Description

---
+++ uwrt_mars_rover_drivetrain/uwrt_mars_rover_drivetrain_hw/src/uwrt_mars_rover_drivetrain_hw.cpp
@@ -0,0 +1,11 @@
+#include <uwrt_mars_rover_drivetrain_hw/uwrt_mars_rover_drivetrain_hw.hpp>
+
+
+namespace uwrt_mars_rover_drivetrain_hw {
+
+UWRTMarsRoverDrivetrainHardware::configure(const hardware_interface::HardwareInfo &info){
+
+}
+
+
+}

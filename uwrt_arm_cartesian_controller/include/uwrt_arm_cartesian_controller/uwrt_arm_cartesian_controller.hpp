cb10a38830f3def47eea7935f5d8285ed17782a8
younes <yreda@uwaterloo.ca>
2022-02-06 23:39:28 -0500

    code compiles

--- uwrt_arm_cartesian_controller/include/uwrt_arm_cartesian_controller/uwrt_arm_cartesian_controller.hpp
+++ uwrt_arm_cartesian_controller/include/uwrt_arm_cartesian_controller/uwrt_arm_cartesian_controller.hpp
@@ -1,67 +1,68 @@
 #pragma once
 
+#include <memory>
+
 #include "controller_interface/controller_interface.hpp"
+#include "geometry_msgs/msg/twist_stamped.hpp"
+#include "kdl/chainiksolver.hpp"
+#include "kdl/chainiksolvervel_pinv.hpp"
+#include "kdl/tree.hpp"
 #include "rclcpp/subscription.hpp"
 #include "rclcpp_lifecycle/node_interfaces/lifecycle_node_interface.hpp"
 #include "rclcpp_lifecycle/state.hpp"
 #include "realtime_tools/realtime_buffer.h"
-#include "geometry_msgs/msg/twist_stamped.hpp"
 #include "uwrt_arm_cartesian_controller/visibility_control.hpp"
-#include "kdl/tree.hpp"
-#include "kdl/chainiksolver.hpp"
-#include "kdl/chainiksolvervel_pinv.hpp"
 
-#include <memory>
+namespace uwrt_arm_cartesian_controller {
+using TwistStamped = geometry_msgs::msg::TwistStamped;
+using CallbackReturn = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
 
-namespace uwrt_arm_cartesian_controller
-{
-    using TwistStamped = geometry_msgs::msg::TwistStamped;
-    using CallbackReturn = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
+class UWRTCartesianController : public controller_interface::ControllerInterface {
+ public:
+  UWRTCartesianController();
 
-    class UWRTCartesianController : public controller_interface::ControllerInterface
-    {
-    public:
-        UWRTCartesianController();
+  ~UWRTCartesianController();
 
-        ~UWRTCartesianController();
+  UWRT_ARM_CARTESIAN_CONTROLLER_PUBLIC
+  controller_interface::InterfaceConfiguration command_interface_configuration() const override;
 
-        UWRT_ARM_CARTESIAN_CONTROLLER_PUBLIC
-        controller_interface::InterfaceConfiguration command_interface_configuration() const override;
+  UWRT_ARM_CARTESIAN_CONTROLLER_PUBLIC
+  controller_interface::InterfaceConfiguration state_interface_configuration() const override;
 
-        UWRT_ARM_CARTESIAN_CONTROLLER_PUBLIC
-        controller_interface::InterfaceConfiguration state_interface_configuration() const override;
+  UWRT_ARM_CARTESIAN_CONTROLLER_PUBLIC
+  CallbackReturn on_init() override;
 
-        UWRT_ARM_CARTESIAN_CONTROLLER_PUBLIC
-        CallbackReturn on_init() override;
+  UWRT_ARM_CARTESIAN_CONTROLLER_PUBLIC
+  CallbackReturn on_configure(const rclcpp_lifecycle::State &previous_state) override;
 
-        UWRT_ARM_CARTESIAN_CONTROLLER_PUBLIC
-        CallbackReturn on_configure(const rclcpp_lifecycle::State &previous_state) override;
+  UWRT_ARM_CARTESIAN_CONTROLLER_PUBLIC
+  CallbackReturn on_activate(const rclcpp_lifecycle::State &previous_state) override;
 
-        UWRT_ARM_CARTESIAN_CONTROLLER_PUBLIC
-        CallbackReturn on_activate(const rclcpp_lifecycle::State &previous_state) override;
+  UWRT_ARM_CARTESIAN_CONTROLLER_PUBLIC
+  CallbackReturn on_deactivate(const rclcpp_lifecycle::State &previous_state) override;
 
-        UWRT_ARM_CARTESIAN_CONTROLLER_PUBLIC
-        CallbackReturn on_deactivate(const rclcpp_lifecycle::State &previous_state) override;
+  UWRT_ARM_CARTESIAN_CONTROLLER_PUBLIC
+  controller_interface::return_type update(const rclcpp::Time &time, const rclcpp::Duration &period) override;
 
-        UWRT_ARM_CARTESIAN_CONTROLLER_PUBLIC
-        controller_interface::return_type update(const rclcpp::Time &time, const rclcpp::Duration &period) override;
+ protected:
+  rclcpp::Subscription<TwistStamped>::SharedPtr m_cart_command_sub;
 
-    protected:
-        rclcpp::Subscription<TwistStamped>::SharedPtr m_cart_command_sub;
-        // realtime_tools::RealtimeBuffer<std::shared_ptr<TwistStamped>> m_cart_command_buffer; // younes todo dont this buffer. just always use the latest cmd just like aidan did. initialize the twist to 0 vector
+  std::vector<std::string> m_joint_names;
+  std::string m_cart_command_topic, m_robot_description, m_root_name, m_tip_name;
 
-        std::vector<std::string> m_joint_names;
-        std::string m_interface_name, m_cart_command_topic, m_robot_description, m_root_name, m_tip_name;
+ private:
+  bool parse_params();
+  TwistStamped get_null_twist() const;
+  // younes todo need realtime buffer bc subscirber runs in different thread and the copy isnt atomic
 
-    private:
-        bool parse_params();
-        TwistStamped get_null_twist() const;
+  KDL::Tree m_kdl_tree;
+  KDL::Chain m_kdl_chain;
+  std::unique_ptr<KDL::ChainIkSolverVel> m_ik_solver;
 
-        KDL::Tree m_kdl_tree;
-        KDL::Chain m_kdl_chain;
-        std::unique_ptr<KDL::ChainIkSolverVel> m_ik_solver;
+  realtime_tools::RealtimeBuffer<std::shared_ptr<TwistStamped>> m_rt_buffer;
 
-        TwistStamped m_last_twist;
-    };
+  static constexpr auto COMMAND_INTERFACE = "velocity";
+  static constexpr auto STATE_INTERFACE = "position";
+};
 
-} // namespace uwrt_arm_cartesian_controller
+}  // namespace uwrt_arm_cartesian_controller
--- uwrt_arm_cartesian_controller/src/uwrt_arm_cartesian_controller.cpp
+++ uwrt_arm_cartesian_controller/src/uwrt_arm_cartesian_controller.cpp
@@ -8,14 +8,13 @@
 
 namespace uwrt_arm_cartesian_controller
 {
-    UWRTCartesianController::UWRTCartesianController() : m_cart_command_sub(nullptr), m_ik_solver(nullptr) {}
+    UWRTCartesianController::UWRTCartesianController() : m_cart_command_sub(nullptr), m_ik_solver(nullptr), m_rt_buffer(nullptr) {}
 
     CallbackReturn UWRTCartesianController::on_init()
     {
         try
         {
             auto_declare<std::vector<std::string>>("joint_names", std::vector<std::string>());
-            auto_declare<std::string>("interface_name", "");
             auto_declare<std::string>("end_effector_velocity_topic", "");
             auto_declare<std::string>("robot_description", "");
             auto_declare<std::string>("root_name", "");
@@ -39,7 +38,7 @@ namespace uwrt_arm_cartesian_controller
         }
 
         // Generate tree from urdf
-        if (!kdl_parser::treeFromParam(m_robot_description, m_kdl_tree))
+        if (!kdl_parser::treeFromFile(m_robot_description, m_kdl_tree))
         {
             RCLCPP_ERROR(get_node()->get_logger(), "Failed to parse KDL tree from robot description");
             return CallbackReturn::ERROR;
@@ -47,35 +46,37 @@ namespace uwrt_arm_cartesian_controller
 
         // Populate the KDL chain
         const bool chain_generated_successfully = m_kdl_tree.getChain(m_root_name, m_tip_name, m_kdl_chain);
-        std::stringstream tree_info = "\tTree has " << m_kdl_tree.getNrOfJoints() << " joints \n\tThe joint names are:";
+        std::stringstream tree_info;
+        tree_info <<"Tree has " << m_kdl_tree.getNrOfJoints() << " joints" << std::endl;
+        tree_info << "The joint names are: " << std::endl;
         for (auto it = m_kdl_chain.segments.begin(); it != m_kdl_chain.segments.end(); ++it)
         {
-            tree_info << "\n\t\t" << it->getJoint().getName());
+            tree_info << it->getJoint().getName() << std::endl;
         }
-        tree_info << "\n\tTree has " << m_kdl_tree.getNrOfSegments() << " segments"
-                  << "\n\tThe segments are:";
+        tree_info << "Tree has " << m_kdl_tree.getNrOfSegments() << " segments" << std::endl;
+        tree_info << "The segments are:" << std::endl;
         KDL::SegmentMap segment_map = m_kdl_tree.getSegments();
         for (auto it = segment_map.begin(); it != segment_map.end(); it++)
         {
-            tree_info << "\n\t\t" << it->first);
+            tree_info << it->first << std::endl;
         }
 
         if (!chain_generated_successfully)
         {
-            RCLCPP_ERROR(get_node()->get_logger(), "Failed to get KDL chain from tree: \n\t" << m_root_name << " --> " << m_tip_name);
-            RCLCPP_ERROR(get_node()->get_logger(), tree_info.str());
+            RCLCPP_ERROR(get_node()->get_logger(), ("Failed to get KDL chain from tree: " + m_root_name + " --> " + m_tip_name).c_str());
+            RCLCPP_ERROR(get_node()->get_logger(), tree_info.str().c_str());
             return CallbackReturn::ERROR;
         }
 
-        RCLCPP_INFO(get_node()->get_logger(), "Successfully got KDL chain from tree: \n\t" << m_root_name << " --> " << m_tip_name);
-        RCLCPP_INFO(get_node()->get_logger(), tree_info.str());
+        RCLCPP_INFO(get_node()->get_logger(), ("Successfully got KDL chain from tree: " + m_root_name + " --> " + m_tip_name).c_str());
+        RCLCPP_INFO(get_node()->get_logger(), tree_info.str().c_str());
 
         m_ik_solver.reset(new KDL::ChainIkSolverVel_pinv(m_kdl_chain));
 
         m_cart_command_sub = get_node()->create_subscription<TwistStamped>(
             m_cart_command_topic, rclcpp::SystemDefaultsQoS(),
             [this](const TwistStamped::SharedPtr msg)
-            { m_last_twist = *msg; }); // younes todo any chance msg could be null?
+            { m_rt_buffer.writeFromNonRT(msg); }); // younes todo any chance msg could be null?
 
         RCLCPP_INFO(get_node()->get_logger(), "configure successful");
         return CallbackReturn::SUCCESS;
@@ -84,13 +85,12 @@ namespace uwrt_arm_cartesian_controller
     controller_interface::InterfaceConfiguration
     UWRTCartesianController::command_interface_configuration() const
     {
-        // younes todo better understand this. what is command_interfaces_?
         controller_interface::InterfaceConfiguration command_interfaces_config;
         command_interfaces_config.type = controller_interface::interface_configuration_type::INDIVIDUAL;
 
         for (const auto &joint : m_joint_names)
         {
-            command_interfaces_config.names.push_back(joint + "/" + m_interface_name);
+            command_interfaces_config.names.push_back(joint + "/" + COMMAND_INTERFACE);
         }
 
         return command_interfaces_config;
@@ -99,8 +99,15 @@ namespace uwrt_arm_cartesian_controller
     controller_interface::InterfaceConfiguration
     UWRTCartesianController::state_interface_configuration() const
     {
-        return controller_interface::InterfaceConfiguration{
-            controller_interface::interface_configuration_type::NONE};
+        controller_interface::InterfaceConfiguration state_interfaces_config;
+        state_interfaces_config.type = controller_interface::interface_configuration_type::INDIVIDUAL;
+
+        for (const auto &joint : m_joint_names)
+        {
+            state_interfaces_config.names.push_back(joint + "/" + STATE_INTERFACE);
+        }
+
+        return state_interfaces_config;
     }
 
     CallbackReturn UWRTCartesianController::on_activate(
@@ -108,20 +115,26 @@ namespace uwrt_arm_cartesian_controller
     {
         //  check if we have all resources defined in the "points" parameter
         //  also verify that we *only* have the resources defined in the "points" parameter
-        std::vector<std::reference_wrapper<hardware_interface::LoanedCommandInterface>> ordered_interfaces;
-        if (
-            !controller_interface::get_ordered_interfaces(
-                command_interfaces_, m_joint_names, m_interface_name, ordered_interfaces) ||
+        /* younes todo do i need this std::vector<std::reference_wrapper<hardware_interface::LoanedCommandInterface>> ordered_interfaces;
+        if (!controller_interface::get_ordered_interfaces(command_interfaces_, m_joint_names, COMMAND_INTERFACE, ordered_interfaces) ||
             command_interfaces_.size() != ordered_interfaces.size())
         {
             RCLCPP_ERROR(
-                node_->get_logger(), "Expected %zu position command interfaces, got %zu", m_joint_names.size(),
+                node_->get_logger(), "Expected %zu velocity command interfaces, got %zu", m_joint_names.size(),
                 ordered_interfaces.size());
             return CallbackReturn::ERROR;
+        }*/
+
+        if (command_interfaces_.size() != m_joint_names.size() || state_interfaces_.size() != m_joint_names.size())
+        {
+            RCLCPP_ERROR(node_->get_logger(), "Expected %zu velocity command interfaces, got %zu. Expected %zu position state interfaces, got %zu.", m_joint_names.size(),
+                         command_interfaces_.size(), m_joint_names.size(), state_interfaces_.size());
+            return CallbackReturn::ERROR;
+
         }
 
         // reset command if a command came through callback when controller was inactive
-        m_last_twist = get_null_twist();
+        m_rt_buffer.reset();
 
         return CallbackReturn::SUCCESS;
     }
@@ -134,58 +147,57 @@ namespace uwrt_arm_cartesian_controller
             command_interface.set_value(0.0);
         }
 
-        m_last_twist = get_null_twist();
+        m_rt_buffer.reset();
 
         return CallbackReturn::SUCCESS;
     }
 
     controller_interface::return_type UWRTCartesianController::update(const rclcpp::Time &time, const rclcpp::Duration & /*period*/)
     {
-        // expired command
-        if (time - m_last_twist.header.stamp > rclcpp::Duration(5, 0)) // younes todo make this a param
+        auto send_zero_joint_velocities = [this]()
         {
-            KDL::SetToZero(m_cart_vel); // younes todo this var doesnt exist and its a geometry msg not a kdl type
-            for (uint8_t i = 0; i < command_interfaces_.size(); ++i)
-            {
-                command_interface.set_value(0.0);
-            }
+          for (auto &command_interface : command_interfaces_)
+          {
+              command_interface.set_value(0.0);
+          }
+        };
+        const auto end_effector_cart_vel = m_rt_buffer.readFromRT();
+        // no command available or expired command
+        if(!end_effector_cart_vel || !*end_effector_cart_vel || (time - (*end_effector_cart_vel)->header.stamp) > rclcpp::Duration(5, 0))
+        {
+            // younes todo make the duration a param
+            send_zero_joint_velocities();
         }
         else
         {
-            KDL::JntArray joint_positions(command_interfaces_.size());
+            KDL::JntArray joint_positions(state_interfaces_.size());
+            for (uint8_t i = 0; i < state_interfaces_.size(); ++i)
+            {
+                joint_positions(i) = state_interfaces_.at(i).get_value();
+            }
 
             KDL::Twist end_effector_velocity = KDL::Twist::Zero();
-            end_effector_velocity(0) = m_last_twist.twist.linear.x;
-            end_effector_velocity(1) = m_last_twist.twist.linear.y;
-            end_effector_velocity(2) = m_last_twist.twist.linear.z;
-            end_effector_velocity(3) = m_last_twist.twist.angular.x;
-            end_effector_velocity(4) = m_last_twist.twist.angular.y;
-            end_effector_velocity(5) = m_last_twist.twist.angular.z;
+            end_effector_velocity(0) = (*end_effector_cart_vel)->twist.linear.x;
+            end_effector_velocity(1) = (*end_effector_cart_vel)->twist.linear.y;
+            end_effector_velocity(2) = (*end_effector_cart_vel)->twist.linear.z;
+            end_effector_velocity(3) = (*end_effector_cart_vel)->twist.angular.x;
+            end_effector_velocity(4) = (*end_effector_cart_vel)->twist.angular.y;
+            end_effector_velocity(5) = (*end_effector_cart_vel)->twist.angular.z;
 
-            KDL::JntArray joint_velocities(command_interfaces_.size();
-
-            for (uint8_t i = 0; i < command_interfaces_.size(); ++i)
-            {
-                joint_positions(i) = command_interafces_.at(i).getPosition();
-            }
+            KDL::JntArray joint_velocities(command_interfaces_.size());
 
             if (m_ik_solver->CartToJnt(joint_positions, end_effector_velocity, joint_velocities) < 0)
             {
                 RCLCPP_ERROR(get_node()->get_logger(), "Unable to compute joint velocities for given end-effector velocity");
-                KDL::SetToZero(m_cart_vel); // younes todo var doesnt exist anymroe
-                for (uint8_t i = 0; i < command_interfaces_.size(); ++i)
-                {
-                    command_interface.set_value(0.0);
-                }
+                send_zero_joint_velocities();
                 return controller_interface::return_type::ERROR;
             }
             
             for (uint8_t i = 0; i < command_interfaces_.size(); ++i)
             {
-                command_interfaces_.at(i).setCommand(joint_velocities(i)); // younes todo use set_values
+                command_interfaces_.at(i).set_value(joint_velocities(i));
             }
         }
-
         return controller_interface::return_type::OK;
     }
 
@@ -200,39 +212,21 @@ namespace uwrt_arm_cartesian_controller
         }
 
         // Loop over string params
-        const std::array<std::pair<std::reference_wrapper<std::string>, std::string>, 5> string_params = {{m_interface_name, "interface_name"},
-                                                                                                          {m_cart_command_topic, "end_effector_velocity_topic"},
-                                                                                                          {m_robot_description, "robot_description"},
-                                                                                                          {m_root_name, "root_name"},
-                                                                                                          {m_tip_name, "tip_name"}};
+        const std::unordered_map<std::string *, const std::string> string_params = {{&m_cart_command_topic, "end_effector_velocity_topic"},
+                                                                                    {&m_robot_description, "robot_description"},
+                                                                                    {&m_root_name, "root_name"},
+                                                                                    {&m_tip_name, "tip_name"}};
         for (auto &string_param : string_params)
-        {
-            if (string_param.first.empty())
+        {   
+            *(string_param.first) = node_->get_parameter(string_param.second).as_string();
+            if (string_param.first->empty())
             {
-                string_param.first = node_->get_parameter(string_param.second).as_string();
-                if (string_param.first.empty())
-                {
-                    RCLCPP_ERROR(get_node()->get_logger(), string_param.second + " parameter was empty");
-                    return false;
-                }
+                RCLCPP_ERROR(get_node()->get_logger(), (string_param.second + " parameter was empty").c_str());
+                return false;
             }
         }
 
         return true;
-    }
-
-    TwistStamped UWRTCartesianController::get_null_twist() const
-    {
-        TwistStamped ret;
-        ret.twist.linear.x = 0;
-        ret.twist.linear.y = 0;
-        ret.twist.linear.z = 0;
-        ret.twist.angular.x = 0;
-        ret.twist.angular.y = 0;
-        ret.twist.angular.z = 0;
-        ret.header.stamp = rclcpp::Time::now();
-        // younes todo whats the name of the arm's base frame ret.header.frame_id = "foo";
-        return ret;
     }
 
 } // namespace uwrt_arm_cartesian_controller

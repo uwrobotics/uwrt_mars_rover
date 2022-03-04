77c7b3f18478b981e24d661ae91f90f775c384b9
Keyon-Jerome <keyonjerome@gmail.com>
2022-03-03 20:07:09 -0500

    clang format

--- uwrt_mars_rover_utils/include/uwrt_mars_rover_utils/data_utils.hpp
+++ uwrt_mars_rover_utils/include/uwrt_mars_rover_utils/data_utils.hpp
@@ -2,26 +2,27 @@
 
 namespace DifferentialTransmissionData {
 
-
 namespace ActuatorData {
 
-    struct CommandData {
-        double velocity{};  // TODO: change this so that it can work with any interface type... add enum class that will hold
-                            // the different command types
-    };
+struct CommandData {
+  double velocity{};  // TODO: change this so that it can work with any interface type... add enum class that will hold
+                      // the different command types
+};
 
-    struct StateData {
-        double velocity{};
-        double position{};
-        double iq_current{};  // not used for differential transmission
-    };  
+struct StateData {
+  double velocity{};
+  double position{};
+  double iq_current{};  // not used for differential transmission
+};
 
 }  // namespace ActuatorData
 
 // keeping this separate from ActuatorData despite the overlap for extensibility reasons
 namespace JointData {
 
-struct CommandData { double velocity{}; };
+struct CommandData {
+  double velocity{};
+};
 
 struct StateData {
   double velocity{};

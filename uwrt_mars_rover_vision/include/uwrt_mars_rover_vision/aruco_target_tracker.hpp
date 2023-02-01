9700523311391e96d1063a1a0a898177ad133880
nick-palmar <nicolas.palmar8@gmail.com>
2023-02-01 00:13:37 -0500

    complete aruco target tracker

--- uwrt_mars_rover_vision/include/uwrt_mars_rover_vision/aruco_target_tracker.hpp
+++ uwrt_mars_rover_vision/include/uwrt_mars_rover_vision/aruco_target_tracker.hpp
@@ -6,15 +6,10 @@
 #include <geometry_msgs/msg/pose_stamped.hpp>
 #include <sensor_msgs/msg/image.hpp>
 #include <sensor_msgs/msg/camera_info.hpp>
-#include "image_transport/image_transport.hpp"
-#include "opencv2/highgui/highgui.hpp"
-
+#include <image_transport/image_transport.hpp>
+#include <opencv2/highgui/highgui.hpp>
 #include <opencv2/aruco.hpp>
-#include "opencv2/imgproc.hpp"
-#include <opencv2/cvconfig.h>
-
 #include <vector>
-
 
 namespace uwrt_autonomy {
 
@@ -24,38 +19,49 @@ public:
     TargetTracker(const rclcpp::NodeOptions &options);
 
 private:
-    // zed2 image topic subscriber
-    image_transport::Subscriber camera_sub_;
-    // camera subsciber image callback
-    void imageCallback(const sensor_msgs::msg::Image::ConstSharedPtr& msg);
-    
-    // pose publisher for aruco tags
-    rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr aruco_pose_pub_;
+    /**
+     * @brief Reads images from a zed camera subscriber topic and publishes poses for detected ARUCO tags
+     * @param image_msg Image data and metadata from the zed2 camera 
+    */
+    void imageCallback(const sensor_msgs::msg::Image::ConstSharedPtr& image_msg);
 
-    // make subscriber to get the camera info
-    rclcpp::Subscription<sensor_msgs::msg::CameraInfo>::SharedPtr camera_info_sub_;
+    /**
+     * @brief Reads intrinstic calibration data from the camera info topic
+     * @param info Camera info message
+    */
     void camInfoCallback(const sensor_msgs::msg::CameraInfo::SharedPtr info);
 
-    // convert translation and rotation vector to pose message
+    /**
+     * @brief Converts a rotation vector and translation vector to pose message 
+     * @param rvec Compressed rodrigues rotation vector (angle-axis form) which represents a rotation
+     * @param tvec Translation vector in 3D
+     * @param pose_msg Reference to a pose message 
+    */
     void toPoseMsg(cv::Vec3d rvec, cv::Vec3d tvec, geometry_msgs::msg::Pose& pose_msg);
+
+    // pose publisher for aruco tags
+    rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr aruco_pose_pub_;
+    // zed2 image topic subscriber
+    image_transport::Subscriber camera_sub_;
+    // camera info subscriber to get intrinstic calibration params
+    rclcpp::Subscription<sensor_msgs::msg::CameraInfo>::SharedPtr camera_info_sub_;
 
     // zed calibration stuff
     cv::Mat dist_coefficients_ = cv::Mat::zeros(1, 5, CV_32FC1);
     cv::Mat intrinsic_calib_matrix_;
 
-    // vectors for ARUCO tag poses (max of 4 aruco codes identified at a time)
+    // vectors for ARUCO tag rotations and translations (max of 4 aruco codes identified at a time)
     std::vector<cv::Vec3d> rvecs_{std::vector<cv::Vec3d>(4, 0.0)}, tvecs_{std::vector<cv::Vec3d>(4, 0.0)};
-
-    // aruco detector things
+    // opencv aruco detector variables
     cv::Ptr<cv::aruco::DetectorParameters> params_;
     cv::Ptr<cv::aruco::Dictionary> dictionary_;
     // location of corners in object's reference frame 
-    // NOTE: below assumes fixed size for aruco tags
     cv::Mat obj_points_{cv::Mat(4, 1, CV_32FC3)};
 
-    // marker length in meters for ARUCO code. Be sure to change it if the marker length changes
-    // TODO: change this into a configurable parameter
-    float ARUCO_MARKER_LEN = 18.4 / 100;
+    // marker length (both length and width are the same) in meters for an ARUCO marker
+    float aruco_marker_len_;
+    // whether or not to show a visual output of the codes
+    bool display_marker_pose_;
 };
  
 }
--- uwrt_mars_rover_vision/src/aruco_target_tracker.cpp
+++ uwrt_mars_rover_vision/src/aruco_target_tracker.cpp
@@ -12,28 +12,47 @@ namespace uwrt_autonomy {
 
 TargetTracker::TargetTracker(const rclcpp::NodeOptions& options) : Node("target_tracker", options) 
 {
-  // todo: figure out how to only call subscription once to not constantly read these messages
-  // maybe link the camera info and camera subscription messages together? Only do if necessary
-  camera_info_sub_ = create_subscription<sensor_msgs::msg::CameraInfo>(
-      "/zed2/zed_node/left/camera_info", 10, std::bind(&TargetTracker::camInfoCallback, this, std::placeholders::_1));
+  // get parameters set in the launch file
+  this->declare_parameter("aruco_marker_len", 0.2);
+  this->declare_parameter("aruco_dict", "");
+  this->declare_parameter("display_marker_pose", true);
+  aruco_marker_len_ = this->get_parameter("aruco_marker_len").get_parameter_value().get<float>();
+  display_marker_pose_ = this->get_parameter("display_marker_pose").get_parameter_value().get<bool>();
+  std::string aruco_dict = this->get_parameter("aruco_dict").get_parameter_value().get<std::string>();
+  RCLCPP_DEBUG_STREAM(this->get_logger(), "Marker Len: " << aruco_marker_len_);
+
+  camera_info_sub_ = create_subscription<sensor_msgs::msg::CameraInfo>("/zed2/zed_node/left/camera_info", 10, std::bind(&TargetTracker::camInfoCallback, this, std::placeholders::_1));
 
   rmw_qos_profile_t custom_qos = rmw_qos_profile_default;
-  camera_sub_ = image_transport::create_subscription(
-      this, "/zed2/zed_node/left/image_rect_color",
-      std::bind(&TargetTracker::imageCallback, this, std::placeholders::_1), "raw", custom_qos);
+  camera_sub_ = image_transport::create_subscription(this, "/zed2/zed_node/left/image_rect_color", std::bind(&TargetTracker::imageCallback, this, std::placeholders::_1), "raw", custom_qos);
   
   aruco_pose_pub_ = create_publisher<geometry_msgs::msg::PoseStamped>("/aruco_pose", 10);
   
   // do aruco setup stuff
-  obj_points_.ptr<cv::Vec3f>(0)[0] = cv::Vec3f(-ARUCO_MARKER_LEN/2.f, ARUCO_MARKER_LEN/2.f, 0);
-  obj_points_.ptr<cv::Vec3f>(0)[1] = cv::Vec3f(ARUCO_MARKER_LEN/2.f, ARUCO_MARKER_LEN/2.f, 0);
-  obj_points_.ptr<cv::Vec3f>(0)[2] = cv::Vec3f(ARUCO_MARKER_LEN/2.f, -ARUCO_MARKER_LEN/2.f, 0);
-  obj_points_.ptr<cv::Vec3f>(0)[3] = cv::Vec3f(-ARUCO_MARKER_LEN/2.f, -ARUCO_MARKER_LEN/2.f, 0);
+  obj_points_.ptr<cv::Vec3f>(0)[0] = cv::Vec3f(-aruco_marker_len_/2.f, aruco_marker_len_/2.f, 0);
+  obj_points_.ptr<cv::Vec3f>(0)[1] = cv::Vec3f(aruco_marker_len_/2.f, aruco_marker_len_/2.f, 0);
+  obj_points_.ptr<cv::Vec3f>(0)[2] = cv::Vec3f(aruco_marker_len_/2.f, -aruco_marker_len_/2.f, 0);
+  obj_points_.ptr<cv::Vec3f>(0)[3] = cv::Vec3f(-aruco_marker_len_/2.f, -aruco_marker_len_/2.f, 0);
 
-  // TODO: potentially play with detector params
   params_ = cv::aruco::DetectorParameters::create();
-  // TODO: ensure the below predefined dictionary is the correct one
-  dictionary_ = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_4X4_50);
+  if (aruco_dict == "")
+  {
+    RCLCPP_ERROR_STREAM(this->get_logger(), "Please set an ARUCO dictionary to use");
+  }
+  else if (aruco_dict == "4x4_50")
+  {
+    // use this dictionary for URC
+    dictionary_ = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_4X4_50);
+  }
+  else if (aruco_dict == "4x4_100")
+  {
+    // use this dictionary for CIRC
+    dictionary_ = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_4X4_100);
+  }
+  else
+  {
+    RCLCPP_ERROR_STREAM(this->get_logger(), "Please choose a defined ARUCO dictionary");
+  }
 }
 
 void TargetTracker::toPoseMsg(cv::Vec3d rvec, cv::Vec3d tvec, geometry_msgs::msg::Pose& pose_msg) 
@@ -50,11 +69,11 @@ void TargetTracker::toPoseMsg(cv::Vec3d rvec, cv::Vec3d tvec, geometry_msgs::msg
   tf2::toMsg(tf2_transform, pose_msg);
 }
 
-void TargetTracker::imageCallback(const sensor_msgs::msg::Image::ConstSharedPtr& msg) 
+void TargetTracker::imageCallback(const sensor_msgs::msg::Image::ConstSharedPtr& image_msg) 
 {
   cv_bridge::CvImagePtr cv_ptr;
   try {
-    cv_ptr = cv_bridge::toCvCopy(msg, msg->encoding);
+    cv_ptr = cv_bridge::toCvCopy(image_msg, image_msg->encoding);
   } catch (cv_bridge::Exception& e) {
     RCLCPP_ERROR(this->get_logger(), "cv_bridge exception: %s", e.what());
     return;
@@ -62,11 +81,14 @@ void TargetTracker::imageCallback(const sensor_msgs::msg::Image::ConstSharedPtr&
   // receive a bgra8 encoded image, must convert to bgr
   cv::Mat image_rgb;
   cv::cvtColor(cv_ptr->image, image_rgb, cv::COLOR_BGRA2BGR);
+
+  // detect ARUCO markers in the image
   std::vector<int> marker_ids;
   std::vector<std::vector<cv::Point2f>> marker_corners, rejection_candidates;
   cv::aruco::detectMarkers(image_rgb, dictionary_, marker_corners, marker_ids, params_, rejection_candidates);
   int num_markers_detected = marker_corners.size();
-  // identify aruco codes
+  
+  // identify visible aruco marker poses
   if (num_markers_detected > 0) 
   {
     // Calculate pose for each marker
@@ -78,29 +100,33 @@ void TargetTracker::imageCallback(const sensor_msgs::msg::Image::ConstSharedPtr&
       aruco_pose_msg.header.frame_id = std::to_string(marker_ids[i]);
       toPoseMsg(rvecs_[i], tvecs_[i], aruco_pose_msg.pose);
       aruco_pose_pub_->publish(aruco_pose_msg);
-      // draw the detected poses with coordinate axis of 0.2m in length
-      cv::drawFrameAxes(image_rgb, intrinsic_calib_matrix_, dist_coefficients_, rvecs_.at(i), tvecs_.at(i), 0.2);
+      if (display_marker_pose_)
+      {
+        // draw the detected poses with coordinate axis of 0.2m in length
+        cv::drawFrameAxes(image_rgb, intrinsic_calib_matrix_, dist_coefficients_, rvecs_.at(i), tvecs_.at(i), 0.2);
+      }
     }
-
-    // log the points just to check that stuff is making sense
     RCLCPP_DEBUG_STREAM(this->get_logger(), "ARUCO at tvec: " << tvecs_.at(0) << " | rvec: " << rvecs_.at(0));
-    cv::aruco::drawDetectedMarkers(image_rgb, marker_corners, marker_ids);
+    if (display_marker_pose_)
+    {
+      cv::aruco::drawDetectedMarkers(image_rgb, marker_corners, marker_ids);
+    }
   } 
 
-  // TODO: make optional to show the position on an aruco tag
-  cv::imshow("Image window", image_rgb);
-  // handle key event -> make cv2 show a new image on the window every 5ms
-  cv::waitKey(5);
+  if (display_marker_pose_)
+  {
+    cv::imshow("Image window", image_rgb);
+    cv::waitKey(5);
+  }
 }
 
 void TargetTracker::camInfoCallback(const sensor_msgs::msg::CameraInfo::SharedPtr info) 
 {
-  // temporary hacky work around to only read cam info once
+  // hacky work around to only read cam info once
   if (intrinsic_calib_matrix_.size().height == 0)
   {
-  RCLCPP_INFO_STREAM(this->get_logger(), info->distortion_model);
-  RCLCPP_INFO_STREAM(this->get_logger(), "fx: " << info->k[0] << " | cx: " << info->k[2] << " | fy: " << info->k[4] << " | cy: " << info->k[5]);
-  (void)info;
+  RCLCPP_DEBUG_STREAM(this->get_logger(), info->distortion_model);
+  RCLCPP_DEBUG_STREAM(this->get_logger(), "fx: " << info->k[0] << " | cx: " << info->k[2] << " | fy: " << info->k[4] << " | cy: " << info->k[5]);
   int param_num = 0; 
   for (const double &disto_param: info->d)
   {

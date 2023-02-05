199b1f7012a1ccb6e70edaa8db81a73626b27bb5
wang-edward <edawordwang@gmail.com>
2023-02-05 15:21:51 -0500

    add launch file

--- uwrt_mars_rover_science/dinolite/launch/dinolite_launch.py
+++ uwrt_mars_rover_science/dinolite/launch/dinolite_launch.py
@@ -1,1 +1,51 @@
-print("whats good")
+import launch
+import launch_ros.actions
+from launch_ros.actions import Node
+from launch_ros.descriptions import ComposableNode
+from launch_ros.actions import ComposableNodeContainer
+from launch import LaunchDescription
+from launch.actions import (DeclareLaunchArgument, EmitEvent, ExecuteProcess,
+                                    LogInfo, RegisterEventHandler, TimerAction)
+from launch.conditions import IfCondition
+from launch.event_handlers import (OnExecutionComplete, OnProcessExit,
+                                        OnProcessIO, OnProcessStart, OnShutdown)
+from launch.events import Shutdown
+from launch.substitutions import (EnvironmentVariable, FindExecutable,
+                                        LaunchConfiguration, LocalSubstitution,
+                                                                        PythonExpression)
+
+def generate_launch_description():
+    cam_node_container = ComposableNodeContainer(
+            name = 'cam_node_container',
+            namespace = 'cam',
+            package = 'rclcpp_components',
+            executable = 'component_container',
+            composable_node_descriptions = [
+                ComposableNode(
+                    package ='dinolite',
+                    plugin = 'dinolite::CamNode',
+                    name = 'cam_node'),
+            ],
+            output = 'screen',
+    )
+    process_exit = RegisterEventHandler(
+        OnProcessExit(
+            target_action=cam_node_container,
+            on_exit=[
+                LogInfo(msg=(EnvironmentVariable(name='USER'),
+                        ' closed the turtlesim window')),
+                EmitEvent(event=Shutdown(
+                    reason='Window closed'))
+            ]
+        )
+    )
+    shutdown = RegisterEventHandler(
+        OnShutdown(
+            on_shutdown=[LogInfo(
+                msg=['Launch was asked to shutdown: ',
+                    LocalSubstitution('event.reason')]
+            )]
+        )
+    )
+
+    return launch.LaunchDescription([cam_node_container, process_exit, shutdown])

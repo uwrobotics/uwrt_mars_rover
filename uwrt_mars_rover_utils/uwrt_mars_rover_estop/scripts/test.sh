6c980a8aa8e402d1b93f44dc0de11a3503b7256f
Nafis Molla <nafisfardin@gmail.com>
2023-02-08 21:28:52 -0500

    added color to terminal

--- uwrt_mars_rover_utils/uwrt_mars_rover_estop/scripts/test.sh
+++ uwrt_mars_rover_utils/uwrt_mars_rover_estop/scripts/test.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 while true; do
-#   This line reads a single character from the keyboard and stores it in the key variable. 
-#   The -n1 option specifies to read only one character, 
-#   the -r option disables backslash interpretation, 
-#   and the -p option is used to display the message Press any key to continue... on the terminal before reading the key.
-#   we store the 
-  read -n1 -r -p "Press any key to continue..." key
-  echo $key
+  echo -e "\033[31mPress ENTER to kill...\033[0m"
+  read -n1 -r key
   ros2 topic pub /estop std_msgs/String "{data: '$key'}" -1
 done
-
 
 
 # In a seperate terminal run 

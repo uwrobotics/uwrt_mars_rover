3e29648200467afdb206b5af27da3ad639338bd1
ColinToft <cwt1078@gmail.com>
2023-02-12 12:43:12 -0500

    Make messages not use hardcoded IDs

--- uwrt_mars_rover_utilities/src/uwrt_can_test_node.cpp
+++ uwrt_mars_rover_utilities/src/uwrt_can_test_node.cpp
@@ -44,9 +44,9 @@ class CanTestNode : public rclcpp::Node {
     void sendCanFloatCallback(const std_msgs::msg::Float64::SharedPtr data) {
         auto msg = (float)data->data;
         if (can_wrapper_float.writeToID<float>(msg, FLOAT_WRITE_ID)) {
-            RCLCPP_INFO(this->get_logger(), "Successfully sent float msg '%f' to id 0x05", msg);
+            RCLCPP_INFO(this->get_logger(), "Successfully sent float msg '%f' to id %x", msg, FLOAT_WRITE_ID);
         } else {
-            RCLCPP_INFO(this->get_logger(), "Failed to send float msg '%f' to id 0x05", msg);
+            RCLCPP_INFO(this->get_logger(), "Failed to send float msg '%f' to id %x", msg, FLOAT_WRITE_ID);
         }
     }
 
@@ -54,9 +54,9 @@ class CanTestNode : public rclcpp::Node {
     void sendCanUIntCallback(const std_msgs::msg::UInt64::SharedPtr data) {
         auto msg = (uint64_t)data->data;
         if (can_wrapper_int.writeToID<uint64_t>(msg, UINT_WRITE_ID)) {
-            RCLCPP_INFO(this->get_logger(), "Successfully sent uint64_t msg '%lu' to id 0x06", msg);
+            RCLCPP_INFO(this->get_logger(), "Successfully sent uint64_t msg '%lu' to id %x", msg, UINT_WRITE_ID);
         } else {
-            RCLCPP_INFO(this->get_logger(), "Failed to send uint64_t msg '%lu' to id 0x06", msg);
+            RCLCPP_INFO(this->get_logger(), "Failed to send uint64_t msg '%lu' to id %x", msg, UINT_WRITE_ID);
         }
     }
 
@@ -66,7 +66,7 @@ class CanTestNode : public rclcpp::Node {
         for (const auto& id : float_ids) {
             float data;
             if (can_wrapper_float.getLatestFromID<float>(data, id)) {
-                RCLCPP_INFO(this->get_logger(), "Successfully read float msg '%f' from id '%d'", data, id);
+                RCLCPP_INFO(this->get_logger(), "Successfully read float msg '%f' from id '%x'", data, id);
             }
         }
 
@@ -74,7 +74,7 @@ class CanTestNode : public rclcpp::Node {
         for (const auto& id : uint_ids) {
             uint64_t data;
             if (can_wrapper_int.getLatestFromID<uint64_t>(data, id)) {
-                RCLCPP_INFO(this->get_logger(), "Successfully read uint64_t msg '%lu' from id '%d'", data, id);
+                RCLCPP_INFO(this->get_logger(), "Successfully read uint64_t msg '%lu' from id '%x'", data, id);
             }
         }
     }

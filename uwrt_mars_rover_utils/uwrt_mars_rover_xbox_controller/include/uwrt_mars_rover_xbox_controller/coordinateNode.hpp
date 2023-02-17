eafdf39a5ec76b91f635ccd2c84e062c1492a98c
skhanna <skhanna@versa-networks.com>
2023-02-17 02:03:25 +0000

    fixing header

--- uwrt_mars_rover_utils/uwrt_mars_rover_xbox_controller/include/uwrt_mars_rover_xbox_controller/coordinateNode.hpp
+++ uwrt_mars_rover_utils/uwrt_mars_rover_xbox_controller/include/uwrt_mars_rover_xbox_controller/coordinateNode.hpp
@@ -1,19 +1,21 @@
 #ifndef COORDINATENODE_HPP_
 #define COORDINATENODE_HPP_
 
-
-#include rclcpp/rclcpp.hpp
+#include <rclcpp/rclcpp.hpp>
 #include <uwrt_mars_rover_xbox_controller/visibility.h>
+#include <uwrt_mars_rover_xbox_controller/msg/xbox_controller.hpp>
+#include <geometry_msgs/msg/twist.hpp>
 
 namespace drivetraincontrollerComposition {
     class CoordinateNode : public rclcpp::Node {
         public:
-            UWRT_MARS_ROVER_COORDINATENODE_PUBLIC
-            explicit CoordinateNode(const rclcpp::NodeOptions &options)
+            UWRT_MARS_ROVER_XBOX_CONTROLLER_PUBLIC
+            explicit CoordinateNode(const rclcpp::NodeOptions &options);
 
         private:
-            rclcpp::Subscription<uwrt_mars_rover_xbox_controller::msg::XboxController>::SharedPtr sub_joy_;
-    }
+            rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr pub_;
+            rclcpp::Subscription<uwrt_mars_rover_xbox_controller::msg::XboxController>::SharedPtr drvtrain_joy_sub;
+    };
 }
 
 #endif

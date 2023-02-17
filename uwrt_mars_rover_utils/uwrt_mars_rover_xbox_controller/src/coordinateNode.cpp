1645e26c1250354511bbce41b9fc3f44b1719e9a
skhanna <skhanna@versa-networks.com>
2023-02-17 02:05:16 +0000

    fixing node

--- uwrt_mars_rover_utils/uwrt_mars_rover_xbox_controller/src/coordinateNode.cpp
+++ uwrt_mars_rover_utils/uwrt_mars_rover_xbox_controller/src/coordinateNode.cpp
@@ -1,23 +1,21 @@
-#include <uwrt_mars_rover_xbox_controller/coordinateNode.h>
-
-#include "rclcpp/rclcpp.hpp"
+#include <uwrt_mars_rover_xbox_controller/coordinateNode.hpp>
 
 namespace drivetraincontrollerComposition {
     CoordinateNode::CoordinateNode(const rclcpp::NodeOptions &options): Node("coordinateNode", options) {
         auto callback =
-            [this][const uwrt_mars_rover_xbox_controller::msg::XboxController::SharedPtr msg_in] -> void {
+            [this](const uwrt_mars_rover_xbox_controller::msg::XboxController::SharedPtr msg_in) -> void {
                 //Add scaling value
-                const vConstant = 5;
+                const int vConstant = 5;
 
                 auto msg = std::make_unique<geometry_msgs::msg::Twist>();
-                msg->linear.x = CoordinateNode::coordinates::linear::x*vConstant;
-                msg->linear.y = CoordinateNode::coordinates::linear::y*vConstant;
+                msg->linear.x = (*msg_in).drivetrain_joy_x*vConstant;
+                msg->linear.y = (*msg_in).drivetrain_joy_y*vConstant;
 
-                this->publisher->publish(std::move(msg));
-            }
+                pub_->publish(std::move(msg));
+            };
         
-        this->publisher = this->create_publisher<geometry_msgs::msg::Twist>("/differential_drivetrain_controller/cmd_vel", 10);
-        sub_joy = create_subscription<uwrt_mars_rover_xbox_controller::msg::XboxController>("/xbox_info", 10, callback);
+        pub_ = create_publisher<geometry_msgs::msg::Twist>("/differential_drivetrain_controller/cmd_vel", 10);
+        drvtrain_joy_sub = create_subscription<uwrt_mars_rover_xbox_controller::msg::XboxController>("/xbox_info", 10, callback);
         
     }
 }

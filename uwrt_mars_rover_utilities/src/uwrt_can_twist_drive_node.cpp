d936f641834a1483b685acf3fad040c0a6fa4d06
Keyon Jerome <keyonjerome@gmail.com>
2023-02-19 05:11:44 -0500

    add twist drive node and launch file

--- uwrt_mars_rover_utilities/src/uwrt_can_twist_drive_node.cpp
+++ uwrt_mars_rover_utilities/src/uwrt_can_twist_drive_node.cpp
@@ -1,11 +1,8 @@
 #include <rclcpp/rclcpp.hpp>
 #include <std_msgs/msg/float32.hpp>
 #include <std_msgs/msg/u_int64.hpp>
+#include <geometry_msgs/msg/twist.hpp>
 #include <uwrt_msgs/msg/set_vel.hpp>
-
-
-static constexpr uint32_t[3] DRIVETRAIN_LEFT_ODRIVE_IDs = {0x001,0x002,0x003};
-static constexpr uint32_t[3] DRIVETRAIN_RIGHT_ODRIVE_IDs = {0x004,0x005,0x006};
 
 // ros constants
 static constexpr int TOPIC_BUFFER_SIZE = 10;
@@ -26,12 +23,23 @@ class CANTwistDriveNode : public rclcpp::Node {
 
 
     void sendTwistToMotors(const geometry_msgs::msg::Twist data) {
-        uwrt_msgs::msg::SetVel motor_msg = uwrt_msgs::msg::SetVel();
-        motor_msg.vel = 
-        auto IDs = {}
+        
+        uint8_t IDs[6] = {uwrt_msgs::msg::SetVel::MOTOR_1_ID,uwrt_msgs::msg::SetVel::MOTOR_2_ID,uwrt_msgs::msg::SetVel::MOTOR_3_ID,
+                    uwrt_msgs::msg::SetVel::MOTOR_4_ID,uwrt_msgs::msg::SetVel::MOTOR_5_ID,uwrt_msgs::msg::SetVel::MOTOR_6_ID};
+        
         // assuming CAN IDs 1-3 are the left side motors, and CAN IDs 4-6 are the right side motors;
+        uwrt_msgs::msg::SetVel motor_msg = uwrt_msgs::msg::SetVel();
+        // Send message to left side motors
         for(int i = 0; i < 3; i++) {
-
+            motor_msg.can_id = IDs[i];
+            motor_msg.vel = data.linear.x - data.linear.y;
+            this->vel_pub->publish(motor_msg);
+        }
+        // Send message to right side motors
+        for(int i = 3; i < 6; i++) {
+            motor_msg.can_id = IDs[i];
+            motor_msg.vel = data.linear.x + data.linear.y;
+            this->vel_pub->publish(motor_msg);
         }
     }
 
@@ -41,8 +49,8 @@ public:
 
 
         // create subscribers
-        subscription_uint = this->create_subscription<std_msgs::msg::UInt64>(
-            "can_test_uint", TOPIC_BUFFER_SIZE, std::bind(&CanTestNode::sendCanUIntCallback, this, std::placeholders::_1));
+        twist_sub = this->create_subscription<geometry_msgs::msg::Twist>(
+            "teleop_twist_joy/cmd_vel", TOPIC_BUFFER_SIZE, std::bind(&CANTwistDriveNode::sendTwistToMotors, this, std::placeholders::_1));
         vel_pub = this->create_publisher<uwrt_msgs::msg::SetVel>(
             "can_send_vel", TOPIC_BUFFER_SIZE);
     }

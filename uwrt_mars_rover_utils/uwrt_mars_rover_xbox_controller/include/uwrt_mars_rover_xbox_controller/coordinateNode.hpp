9b293da2dea7ce2ea62bef0ece3db37e51f0a805
skhanna <skhanna@versa-networks.com>
2023-02-19 19:45:19 +0000

    files

--- uwrt_mars_rover_utils/uwrt_mars_rover_xbox_controller/include/uwrt_mars_rover_xbox_controller/coordinateNode.hpp
+++ uwrt_mars_rover_utils/uwrt_mars_rover_xbox_controller/include/uwrt_mars_rover_xbox_controller/coordinateNode.hpp
@@ -1,21 +1,25 @@
 #ifndef COORDINATENODE_HPP_
 #define COORDINATENODE_HPP_
 
-#include <rclcpp/rclcpp.hpp>
 #include <uwrt_mars_rover_xbox_controller/visibility.h>
-#include <uwrt_mars_rover_xbox_controller/msg/xbox_controller.hpp>
+
 #include <geometry_msgs/msg/twist.hpp>
+#include <rclcpp/rclcpp.hpp>
+#include <uwrt_mars_rover_xbox_controller/msg/xbox_controller.hpp>
 
-namespace drivetraincontrollerComposition {
-    class CoordinateNode : public rclcpp::Node {
-        public:
-            UWRT_MARS_ROVER_XBOX_CONTROLLER_PUBLIC
-            explicit CoordinateNode(const rclcpp::NodeOptions &options);
+namespace drivetraincontrollerComposition
+{
+class CoordinateNode : public rclcpp::Node
+{
+public:
+  UWRT_MARS_ROVER_XBOX_CONTROLLER_PUBLIC
+  explicit CoordinateNode(const rclcpp::NodeOptions & options);
 
-        private:
-            rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr pub_;
-            rclcpp::Subscription<uwrt_mars_rover_xbox_controller::msg::XboxController>::SharedPtr drvtrain_joy_sub;
-    };
-}
+private:
+  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr pub_;
+  rclcpp::Subscription<uwrt_mars_rover_xbox_controller::msg::XboxController>::SharedPtr
+    drvtrain_joy_sub;
+};
+}  // namespace drivetraincontrollerComposition
 
 #endif
--- uwrt_mars_rover_utils/uwrt_mars_rover_xbox_controller/src/coordinateNode.cpp
+++ uwrt_mars_rover_utils/uwrt_mars_rover_xbox_controller/src/coordinateNode.cpp
@@ -1,24 +1,28 @@
 #include <uwrt_mars_rover_xbox_controller/coordinateNode.hpp>
 
-namespace drivetraincontrollerComposition {
-    CoordinateNode::CoordinateNode(const rclcpp::NodeOptions &options): Node("coordinateNode", options) {
-        auto callback =
-            [this](const uwrt_mars_rover_xbox_controller::msg::XboxController::SharedPtr msg_in) -> void {
-                //Add scaling value
-                const int vConstant = 5;
+namespace drivetraincontrollerComposition
+{
+CoordinateNode::CoordinateNode(const rclcpp::NodeOptions & options)
+: Node("coordinateNode", options)
+{
+  auto callback =
+    [this](const uwrt_mars_rover_xbox_controller::msg::XboxController::SharedPtr msg_in) -> void {
+    //Add scaling value
+    const int vConstant = 5;
 
-                auto msg = std::make_unique<geometry_msgs::msg::Twist>();
-                msg->linear.x = (*msg_in).drivetrain_joy_x*vConstant;
-                msg->linear.y = (*msg_in).drivetrain_joy_y*vConstant;
+    auto msg = std::make_unique<geometry_msgs::msg::Twist>();
+    msg->linear.x = (*msg_in).drivetrain_joy_x * vConstant;
+    msg->linear.y = (*msg_in).drivetrain_joy_y * vConstant;
 
-                pub_->publish(std::move(msg));
-            };
-        
-        pub_ = create_publisher<geometry_msgs::msg::Twist>("/differential_drivetrain_controller/cmd_vel", 10);
-        drvtrain_joy_sub = create_subscription<uwrt_mars_rover_xbox_controller::msg::XboxController>("/xbox_info", 10, callback);
-        
-    }
+    pub_->publish(std::move(msg));
+  };
+
+  pub_ =
+    create_publisher<geometry_msgs::msg::Twist>("/differential_drivetrain_controller/cmd_vel", 10);
+  drvtrain_joy_sub = create_subscription<uwrt_mars_rover_xbox_controller::msg::XboxController>(
+    "/xbox_info", 10, callback);
 }
+}  // namespace drivetraincontrollerComposition
 
 #include <rclcpp_components/register_node_macro.hpp>
 RCLCPP_COMPONENTS_REGISTER_NODE(drivetraincontrollerComposition::CoordinateNode)

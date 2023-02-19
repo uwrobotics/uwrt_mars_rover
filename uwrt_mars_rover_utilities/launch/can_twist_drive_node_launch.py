49ec12aa360bd26e91d14e29fad10bbf9e46b347
Keyon Jerome <keyonjerome@gmail.com>
2023-02-19 12:21:45 -0500

    User/keyonjerome/can vel twist (#237)
    * broken twist node
    
    * add twist drive node and launch file

---
+++ uwrt_mars_rover_utilities/launch/can_twist_drive_node_launch.py
@@ -0,0 +1,38 @@
+"""Launch Module for starting drivetrain on real hardware."""
+from ament_index_python.packages import get_package_share_path
+
+from launch import LaunchDescription
+from launch.actions import RegisterEventHandler
+from launch.event_handlers import OnProcessExit
+from launch.substitutions import Command
+
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+
+
+def generate_launch_description():
+    """
+    Generate launch description from list of nodes to launch.
+
+    :return:
+        LaunchDescription object
+    """
+    nodes = []
+
+    nodes += [Node(
+        package='uwrt_mars_rover_utilities',
+        executable='uwrt_can_test_node',
+        parameters=[
+        {"CAN_interface":"can0"}],
+        namespace='',
+        name='can_test_node'
+    )]
+
+    nodes += [Node(
+        package='uwrt_mars_rover_utilities',
+        executable='uwrt_can_twist_drive_node',
+        namespace='',
+        name='can_twist_drive_node'
+    )]
+
+    return LaunchDescription(nodes)
---
+++ uwrt_mars_rover_utilities/src/uwrt_can_twist_drive_node.cpp
@@ -0,0 +1,64 @@
+#include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/float32.hpp>
+#include <std_msgs/msg/u_int64.hpp>
+#include <geometry_msgs/msg/twist.hpp>
+#include <uwrt_msgs/msg/set_vel.hpp>
+
+// ros constants
+static constexpr int TOPIC_BUFFER_SIZE = 10;
+static constexpr int LOOP_RATE = 10;
+
+using namespace std::chrono_literals;
+
+class CANTwistDriveNode : public rclcpp::Node {
+
+    // Subscriptions
+    rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr twist_sub;
+    
+    // Publishers
+    rclcpp::Publisher<uwrt_msgs::msg::SetVel>::SharedPtr vel_pub;
+
+    // Timer to periodically read from CAN bus
+    rclcpp::TimerBase::SharedPtr timer;
+
+
+    void sendTwistToMotors(const geometry_msgs::msg::Twist data) {
+        
+        uint8_t IDs[6] = {uwrt_msgs::msg::SetVel::MOTOR_1_ID,uwrt_msgs::msg::SetVel::MOTOR_2_ID,uwrt_msgs::msg::SetVel::MOTOR_3_ID,
+                    uwrt_msgs::msg::SetVel::MOTOR_4_ID,uwrt_msgs::msg::SetVel::MOTOR_5_ID,uwrt_msgs::msg::SetVel::MOTOR_6_ID};
+        
+        // assuming CAN IDs 1-3 are the left side motors, and CAN IDs 4-6 are the right side motors;
+        uwrt_msgs::msg::SetVel motor_msg = uwrt_msgs::msg::SetVel();
+        // Send message to left side motors
+        for(int i = 0; i < 3; i++) {
+            motor_msg.can_id = IDs[i];
+            motor_msg.vel = data.linear.x - data.linear.y;
+            this->vel_pub->publish(motor_msg);
+        }
+        // Send message to right side motors
+        for(int i = 3; i < 6; i++) {
+            motor_msg.can_id = IDs[i];
+            motor_msg.vel = data.linear.x + data.linear.y;
+            this->vel_pub->publish(motor_msg);
+        }
+    }
+
+
+public:
+    CANTwistDriveNode() : Node("CAN_twist_drive_node") {
+
+
+        // create subscribers
+        twist_sub = this->create_subscription<geometry_msgs::msg::Twist>(
+            "teleop_twist_joy/cmd_vel", TOPIC_BUFFER_SIZE, std::bind(&CANTwistDriveNode::sendTwistToMotors, this, std::placeholders::_1));
+        vel_pub = this->create_publisher<uwrt_msgs::msg::SetVel>(
+            "can_send_vel", TOPIC_BUFFER_SIZE);
+    }
+};
+
+int main(int argc, char* argv[]) {
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<CANTwistDriveNode>());
+    rclcpp::shutdown();
+    return 0;
+}

b7188a7ed84a42e1d1c54cfbd35d51e5a4a0d1a9
Nafis Molla <nafisfardin@gmail.com>
2023-10-03 19:08:40 -0400

    squashed commits

--- uwrt_mars_rover_drivetrain/uwrt_mars_rover_drivetrain_description/launch/gazebo.launch.py
+++ uwrt_mars_rover_drivetrain/uwrt_mars_rover_drivetrain_description/launch/gazebo.launch.py
@@ -1,33 +1,57 @@
-"""Launch Module for starting drivetrain on real hardware."""
-from ament_index_python.packages import get_package_share_path
-
+import os
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+import xacro
+
+
+from ament_index_python.packages import get_package_share_path
 from launch.actions import RegisterEventHandler
 from launch.event_handlers import OnProcessExit
 from launch.substitutions import Command
-
-from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterValue
 
 
 def generate_launch_description():
-    """
-    Generate launch description from list of nodes to launch.
+    #this is launching gazebo
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py']),
+        )
 
-    :return:
-        LaunchDescription object
-    """
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+                arguments=['-topic', 'robot_description',
+                            '-entity', 'my_bot'],
+                output='screen')
+    #-------------------------------------------------------------------------------------
+
+
     drivetrain_description_package_path = get_package_share_path('uwrt_mars_rover_drivetrain_description')
     model_path = drivetrain_description_package_path / 'urdf' / 'drivetrain.urdf.xacro'
     rviz_config_path = drivetrain_description_package_path / 'rviz' / 'urdf.rviz'
     controllers_config_path = get_package_share_path(
         'uwrt_mars_rover_drivetrain_hw') / 'config' / 'drivetrain_controllers.yaml'
 
-    robot_description_content = ParameterValue(Command(['ros2 run xacro xacro ', str(model_path),' sim:=', 'false']), value_type=str)
+    robot_description_content = ParameterValue(Command(['ros2 run xacro xacro ', str(model_path)]), value_type=str)
     robot_description = {'robot_description': robot_description_content}
 
     # Nodes
     nodes = []
+    nodes += [gazebo]
+    nodes += [spawn_entity]
+
+    nodes += [Node(
+        package='controller_manager',
+        executable='ros2_control_node',
+        parameters=[robot_description, controllers_config_path],
+        output={
+            'stdout': 'screen',
+            'stderr': 'screen',
+        },
+    )]  # TODO: use custom control node w/ RT scheduling(port from ros1 uwrt_mars_rover branch)
+
     nodes += [Node(
         package='controller_manager',
         executable='ros2_control_node',
@@ -59,7 +83,7 @@ def generate_launch_description():
         package='rviz2',
         executable='rviz2',
         name='rviz2',
-        output='screen',
+        output='',
         arguments=['-d', str(rviz_config_path)],
     )
     nodes += [RegisterEventHandler(
@@ -82,4 +106,12 @@ def generate_launch_description():
         )
     )]
 
-    return LaunchDescription(nodes)
+    #-------------------------------------------------------------------------------------
+
+
+
+
+    # Run the node
+    return LaunchDescription(
+        nodes
+    )

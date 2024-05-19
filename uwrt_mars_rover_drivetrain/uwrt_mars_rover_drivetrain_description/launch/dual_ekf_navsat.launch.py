17c4957f91e3431810354be02193b388ffeec0af
nico-palmar <nicolas.palmar8@gmail.com>
2024-05-18 21:56:05 -0400

    fuse odometry data using robot localization

---
+++ uwrt_mars_rover_drivetrain/uwrt_mars_rover_drivetrain_description/launch/dual_ekf_navsat.launch.py
@@ -0,0 +1,32 @@
+from launch import LaunchDescription
+from ament_index_python.packages import get_package_share_directory
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+import launch_ros.actions
+import os
+import launch.actions
+
+
+def generate_launch_description():
+    gps_wpf_dir = os.path.join(get_package_share_directory('uwrt_mars_rover_drivetrain_description'))
+    rl_params_file = os.path.join(
+        gps_wpf_dir, "config", "dual_ekf_navsat_params.yaml")
+
+    return LaunchDescription(
+        [
+            launch.actions.DeclareLaunchArgument(
+                "output_final_position", default_value="false"
+            ),
+            launch.actions.DeclareLaunchArgument(
+                "output_location", default_value="~/dual_ekf_navsat_example_debug.txt"
+            ),
+            launch_ros.actions.Node(
+                package="robot_localization",
+                executable="ekf_node",
+                name="ekf_filter_node_odom",
+                output="screen",
+                parameters=[rl_params_file, {"use_sim_time": True}],
+                remappings=[("odometry/filtered", "odometry/local")],
+            ),
+        ]
+    )
